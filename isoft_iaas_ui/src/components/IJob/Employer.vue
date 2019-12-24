ac33c84ccd834811339ae41b649a08d78a3ae84c
389093982 <389093982@qq.com>
2019-12-24 21:12:00 +0800

    job 优化

--- isoft_iaas_ui/src/components/IJob/Employer.vue
+++ isoft_iaas_ui/src/components/IJob/Employer.vue
@@ -1,29 +1,65 @@
 <template>
   <div>
     <div class="isoft_bg_white isoft_pd10">
-      <p>公司名称：
-        <Input class="input" v-if="editMode" v-model="formInline.corporate_ame"/>
-        <span v-else>{{formInline.corporate_ame}}</span>
+      <p class="clear">
+        <label for="corporate_name">公司名称：</label>
+        <input id="corporate_name" class="input" v-if="editMode" v-model="formInline.corporate_name" placeholder="请您输入公司名称"/>
+        <span v-else>{{formInline.corporate_name}}</span>
       </p>
-      <p>公司主页：
-        <Input class="input" v-if="editMode" v-model="formInline.corporate_site"/>
+      <p class="clear">
+        <label for="corporate_site">公司主页：</label>
+        <input id="corporate_site" class="input" v-if="editMode" v-model="formInline.corporate_site" placeholder="请您输入公司主页"/>
         <span v-else>{{formInline.corporate_site}}</span>
       </p>
-      <p>公司规模：
-        <Input class="input" v-if="editMode" v-model="formInline.corporate_size"/>
+      <p class="clear">
+        <label for="corporate_logo">公司 logo：</label>
+        <input id="corporate_logo" readonly="readonly"
+               class="input" v-if="editMode" v-model="formInline.corporate_logo" placeholder="请您上传公司 logo"/>
+        <span v-else>{{formInline.corporate_logo}}</span>
+      </p>
+      <p class="clear">
+        <label for="corporate_size">公司规模：</label>
+        <input id="corporate_size" readonly="readonly"
+               class="input" v-if="editMode" v-model="formInline.corporate_size" placeholder="请您输入公司规模"/>
         <span v-else>{{formInline.corporate_size}}</span>
       </p>
-      <p>招聘类型：
-        <Input class="input" v-if="editMode" v-model="formInline.job_type"/>
+      <p class="clear">
+        <label for="job_type">招聘类型：</label>
+        <input id="job_type" readonly="readonly"
+               class="input" v-if="editMode" v-model="formInline.job_type" placeholder="请您选择招聘类型"/>
         <span v-else>{{formInline.job_type}}</span>
+      </p>
+      <p class="clear">
+        <label for="salary_range">薪酬范围：</label>
+        <input id="salary_range" readonly="readonly"
+               class="input" v-if="editMode" v-model="formInline.salary_range"  placeholder="请您选择薪酬范围"/>
+        <span v-else>{{formInline.salary_range}}</span>
       </p>
 
       <Button style="position: relative;float: right;right: 10px;bottom: 35px;" @click="editMode = !editMode">编辑模式</Button>
     </div>
 
     <div class="isoft_bg_white isoft_pd10 isoft_top10">
-      <p>公司简介：北京博思致新互联网科技有限责任公司xxxxxx</p>
-      <p>职位简介：xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+      <p class="clear">
+        <label for="corporate_desc">公司简介：</label>
+        <textarea rows="5" id="corporate_desc"
+               class="input" v-if="editMode" v-model="formInline.corporate_desc" placeholder="请您输入公司简介"/>
+        <span v-else>{{formInline.corporate_desc}}</span>
+      </p>
+      <p class="clear">
+        <label for="job_desc">职位简介：</label>
+        <textarea rows="5" id="job_desc"
+                  class="input" v-if="editMode" v-model="formInline.job_desc" placeholder="请您输入职位简介"/>
+        <span v-else>{{formInline.job_desc}}</span>
+      </p>
+    </div>
+
+    <div class="isoft_bg_white isoft_pd10 isoft_top10">
+      <p class="clear">
+        <label for="corporate_address">公司地址：</label>
+        <input id="corporate_address" class="input" v-if="editMode" v-model="formInline.corporate_address" placeholder="请您选择公司地址"/>
+        <span v-else>{{formInline.corporate_address}}</span>
+      </p>
     </div>
   </div>
 </template>
@@ -35,10 +71,15 @@
       return {
         editMode:false,
         formInline: {
-          corporate_ame: '待完善',
-          corporate_site: '待完善',
-          corporate_size: '待完善',
-          job_type: '待完善',
+          corporate_name: null,
+          corporate_site: null,
+          corporate_logo: null,
+          corporate_size: null,
+          salary_range: null,
+          job_type: null,
+          corporate_desc: null,
+          job_desc: null,
+          corporate_address: null,
         },
       }
     }
@@ -46,5 +87,27 @@
 </script>
 
 <style scoped>
-
+  p{
+    margin-top: 5px;
+  }
+  label{
+    width: 100px;
+    float: left;
+  }
+  input,textarea{
+    outline-style: none;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    padding: 3px 3px;
+    width: 450px;
+    font-size: 14px;
+    font-family: 'Microsoft Yahei', 'PingFangSC', sans-serif;
+  }
+  /* 设置输入框点击发光效果 */
+  input:focus,textarea:focus{
+    border-color: #66afe9;
+    outline: 0;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
+    box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
+  }
 </style>
