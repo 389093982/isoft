b5d91ea94208d0f8f78d29b5da3b65c21da48054
389093982 <389093982@qq.com>
2019-12-25 22:04:30 +0800

    job 优化

--- isoft_iaas_ui/src/components/IJob/CorporateDetail.vue
+++ isoft_iaas_ui/src/components/IJob/CorporateDetail.vue
@@ -1,28 +1,42 @@
 <template>
   <div>
     <div class="isoft_bg_white isoft_pd10">
-      <p class="clear">
-        <span>公司名称：{{formInline.corporate_name}}</span>
-      </p>
-      <p class="clear">
-        <span>公司主页：{{formInline.corporate_site}}</span>
-      </p>
-      <p class="clear">
-        <span>公司 logo：{{formInline.corporate_logo}}</span>
-      </p>
-      <p class="clear">
-        <span>公司规模：{{formInline.corporate_size}}</span>
-      </p>
-      <p class="clear">
-        <span>招聘类型：{{formInline.job_type}}</span>
-      </p>
-      <p class="clear">
-        <span>详细类型：{{formInline.job_type_detail}}</span>
-      </p>
-      <p class="clear">
-        <span>薪酬范围：{{formInline.salary_range}}</span>
-      </p>
-
+      <Row>
+        <Col span="4">
+          <div class="bookImg">
+            <a>
+              <img v-if="formInline.corporate_logo" :src="formInline.corporate_logo" height="160px" width="180px"/>
+              <img v-else src="../../assets/default.png" height="160px" width="140px"/>
+            </a>
+          </div>
+        </Col>
+        <Col span="20">
+          <p class="clear">
+            <span>公司名称：{{formInline.corporate_name}}</span>
+          </p>
+          <p class="clear">
+            <span>公司主页：{{formInline.corporate_site}}</span>
+          </p>
+          <p class="clear">
+            <span>公司规模：{{formInline.corporate_size}}</span>
+          </p>
+          <p class="clear">
+            <span>招聘类型：
+              <Tag v-for="(jobTypeTag, index) in jobTypeTags">{{jobTypeTag}}</Tag>
+            </span>
+          </p>
+          <p class="clear">
+            <span>详细类型：
+              <Tag v-for="(jobTypeDetail, index) in jobTypeDetails">{{jobTypeDetail}}</Tag>
+            </span>
+          </p>
+          <p class="clear">
+            <span>薪酬范围：
+               <Tag v-for="(salaryRange, index) in salaryRanges">{{salaryRange}}</Tag>
+            </span>
+          </p>
+        </Col>
+      </Row>
       <Button style="position: relative;float: right;right: 10px;bottom: 35px;" @click="$router.push({path:'/job/corporate_edit'})">编辑模式</Button>
     </div>
 
@@ -55,6 +69,7 @@
 
 <script>
   import {QueryCorporateDetail} from "../../api"
+  import {checkEmpty, strSplit} from "../../tools";
 
   export default {
     name: "CorporateDetail",
@@ -93,10 +108,28 @@
           this.formInline.corporate_welfare = result.corporate_detail.corporate_welfare;
           this.formInline.corporate_addres = result.corporate_detail.corporate_addres;
         }
+      },
+      getSplitArray(str, defaultVal){
+        if(!checkEmpty(str)){
+          return strSplit(str, ",");
+        }else{
+          return [defaultVal];
+        }
       }
     },
     mounted(){
       this.refreshCorporateDetail();
+    },
+    computed:{
+      jobTypeTags:function () {
+        return this.getSplitArray(this.formInline.job_type, '暂无分类');
+      },
+      jobTypeDetails:function () {
+        return this.getSplitArray(this.formInline.job_type_detail, '暂无详细分类');
+      },
+      salaryRanges:function () {
+        return this.getSplitArray(this.formInline.salary_range, '暂无范围');
+      },
     }
   }
 </script>
