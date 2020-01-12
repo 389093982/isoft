0ae736d6fc20eaa0b8b6b0cdbacf0609717ed1ee
389093982 <389093982@qq.com>
2020-01-12 21:54:36 +0800

    Author:z00000 Reason:代码优化

--- isoft_iaas_ui/src/components/IGood/GoodList.vue
+++ isoft_iaas_ui/src/components/IGood/GoodList.vue
@@ -5,35 +5,38 @@
       <Col span="2">
         <IBeautifulLink @onclick="$router.push({path:'/igood/good_list'})">热销商品</IBeautifulLink>
       </Col>
-      <Col span="2">
+      <Col span="3">
         <IBeautifulLink @onclick="$router.push({path:'/igood/mine/good_list',query:{type:'mine'}})">我的店铺商品</IBeautifulLink>
+      </Col>
+      <Col span="2">
+        <IBeautifulLink @onclick="$router.push({path:'/igood/mine/good_edit'})">发布商品</IBeautifulLink>
       </Col>
     </Row>
 
-    <div style="margin: 20px;">
+    <div style="margin: 0 20px;">
       <Row :gutter="10">
-        <Col span="12" v-for="good in goods">
+        <Col span="12" v-for="good in goods" class="isoft_top10 isoft_pd10">
           <Row style="margin-bottom: 10px;">
-            <Col span="12" style="text-align: center;">
+            <Col span="10" style="text-align: center;">
               <router-link :to="{path:'/igood/good_detail',query:{id:good.id}}">
                 <!-- 长度大于 2 排除空数组 [] -->
-                <img v-if="good.good_images.length > 2" :src="good.good_images | filterFirst" width="180px" height="180px"/>
+                <img v-if="good.good_images.length > 2" :src="good.good_images | filterFirst" width="160px"
+                     height="180px"/>
                 <img v-else src="../../assets/default.png" width="180px" height="180px"/>
               </router-link>
             </Col>
-            <Col span="12">
-              <div style="height: 150px;width: 100%;word-wrap:break-word;word-break:break-all;overflow: hidden;">
-                <p>商品名称：<span><IBeautifulLink>{{good.good_name}}</IBeautifulLink></span></p>
-                <p>商品描述：<span><IBeautifulLink :in-line="false">{{good.good_desc}}</IBeautifulLink></span></p>
+            <Col span="14" style="height: 200px;">
+              <div style="width: 100%;word-wrap:break-word;word-break:break-all;overflow: hidden;">
+                <p class="p1line">商品名称：{{good.good_name}}</p>
+                <p class="p3line isoft_font12">商品描述：{{good.good_desc}}</p>
                 <p>商品价格：<span style="color: red;font-weight: bold;">￥{{good.good_price}}</span></p>
                 <p>卖家姓名：{{good.good_seller}}</p>
                 <p>卖家联系方式：{{good.seller_contact}}</p>
               </div>
-              <div>
-
+              <div style="position: absolute;right: 0;bottom: 0;">
                 <Button v-if="editable(good)" @click="$router.push({path:'/igood/mine/good_edit',query:{id:good.id}})">编辑商品</Button>
                 <span v-else>
-                  <Button @click="payConfirm(good)">立即购买</Button>
+                  <Button size="small" @click="payConfirm(good)">立即购买</Button>
                 </span>
               </div>
             </Col>
@@ -41,17 +44,13 @@
         </Col>
       </Row>
     </div>
-
-    <div style="text-align: right;margin: 20px 100px 50px 0;">
-      <Button @click="$router.push({path:'/igood/mine/good_edit'})">发布商品</Button>
-    </div>
   </div>
 </template>
 
 <script>
   import IBeautifulLink from "../Common/link/IBeautifulLink";
-  import {GoodList,NewOrder} from "../../api"
-  import {CheckHasLogin,GetLoginUserName} from "../../tools"
+  import {GoodList, NewOrder} from "../../api"
+  import {CheckHasLogin, GetLoginUserName} from "../../tools"
 
   export default {
     name: "GoodList",
