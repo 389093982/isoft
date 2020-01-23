e071060a46d861718c16af2daab0cc62552f1c53
zhoupeng <1875112921@qq.com>
2020-01-23 14:50:23 +0800

    先提交一版，接下里画主页

--- isoft_printlove_ui/src/router/index.js
+++ isoft_printlove_ui/src/router/index.js
@@ -1,15 +1,24 @@
+//==============================================开始引入组件============================================================
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const baseLayout = () => import('../components/base/baseLayout');
+const index = () => import('../components/showPage/index');
 
-Vue.use(Router)
+Vue.use(Router);
 
+//==============================================引入二级模块============================================================
+//首页
+const printloveRouters = [
+  {path:'index',component:index},
+];
+
+
+//==============================================引入一级模块============================================================
 export default new Router({
+  //去掉#号
+  mode:'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path:'/printlove',component:baseLayout,children:printloveRouters},//首页
+    { path: '*', redirect: '/printlove/index' },//首页
   ]
 })
