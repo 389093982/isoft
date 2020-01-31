22a50cb5818ab471268b7dc33b68abb61a10abe1
zhoupeng <1875112921@qq.com>
2020-01-31 18:16:22 +0800

    printLove提交一版本--界面模型

--- isoft_printlove_ui/src/components/showPage/index.vue
+++ isoft_printlove_ui/src/components/showPage/index.vue
@@ -1,6 +1,115 @@
 <template>
-	<div>
-    <div>hello </div>
+  <div>
+    <div style="background-color: #e3e4e5;padding: 5px 0 5px 30px">
+      <div style="float: left;width: 30%">
+        <span style="color: #ff6900;font-size: 13px">收藏本站</span>
+        <span style="color: #ff6900;font-size: 13px">手机APP</span>
+        <span style="color: #ff6900;font-size: 13px">公众号</span>
+      </div>
+      <div style="float: right;width: 30%">
+        <span style="color: #ff6900;font-size: 13px">消息</span>
+        <span style="color: #ff6900;font-size: 13px">邮件</span>
+        <span style="color: #ff6900;font-size: 13px">在线客服</span>
+        <span style="color: #ff6900;font-size: 13px">充值</span>
+        <span style="color: #ff6900;font-size: 13px">注销</span>
+      </div>
+      <div style="clear: both"></div>
+    </div>
+    <div id="headShow" style="background-color: #f6f6f6 ; padding: 20px 40px 20px 40px">
+      <div id="tagPicture" style="width: 30%;float: left;">
+        <h1 style="color: #ff6900">print love</h1>
+      </div>
+      <div style="width: 70%;float: left;">
+        <input placeholder="搜索..." style="border-style:solid;border-color:#ff537c;width: 500px;height: 35px;padding: 2px 0 2px 2px"/>
+        <button style="border-style:solid;border-color:#ff537c;width:60px;height: 35px;background-color: #ff537c; position: relative;left: -4px;top: 1px">
+          <code style="color: white;cursor: pointer;font-size: 15px;">搜索</code>
+        </button>
+      </div>
+      <div style="clear: both;"></div>
+    </div>
+
+    <div id="contentTitleId" style="background-color: #ff537c; padding: 5px 10px 5px 10px">
+      <span>中央大厅</span>
+      <span>
+        <Dropdown>
+        <a href="javascript:void(0)"><p style="color: white">未登录<Icon type="ios-arrow-down"></Icon></p></a>
+        <DropdownMenu slot="list">
+          <DropdownItem>前往登陆</DropdownItem>
+          <DropdownItem>切换账号</DropdownItem>
+          <DropdownItem>注销</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </span>
+      <span>个人中心</span>
+      <span>lover</span>
+      <span>
+        <Dropdown>
+        <a href="javascript:void(0)"><p style="color: white">礼物<Icon type="ios-arrow-down"></Icon></p></a>
+        <DropdownMenu slot="list">
+          <DropdownItem>礼物商城</DropdownItem>
+          <DropdownItem>我的礼物</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </span>
+      <span>
+        <Dropdown>
+        <a href="javascript:void(0)"><p style="color: white">礼金<Icon type="ios-arrow-down"></Icon></p></a>
+        <DropdownMenu slot="list">
+          <DropdownItem>我的红包</DropdownItem>
+          <DropdownItem>我的优惠券</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </span>
+      <span>
+        <Dropdown>
+        <a href="javascript:void(0)"><p style="color: white">安排<Icon type="ios-arrow-down"></Icon></p></a>
+        <DropdownMenu slot="list">
+          <DropdownItem>结婚</DropdownItem>
+          <DropdownItem>设置定时可见</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </span>
+      <span>
+        <Dropdown>
+        <a href="javascript:void(0)"><p style="color: white">家族<Icon type="ios-arrow-down"></Icon></p></a>
+        <DropdownMenu slot="list">
+          <DropdownItem>关系图谱</DropdownItem>
+          <DropdownItem>聊天会话</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </span>
+
+    </div>
+
+    <div style="margin: 20px 40px 0 20px ;">
+      <div id="menuId" style="width: 13%;float: left;background-color: #f6f4f0;border-radius: 10px;">
+        <div>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+          <p class="menuClass">css教程222</p><hr>
+        </div>
+      </div>
+      <div id="contentId" style="width: 65%;float: left;background-color: #fff;margin: 0 1% 0 1% ">
+        111111111111111111111111111111111111111111111111111111111111111111111111111111111
+      </div>
+      <div id="navigationId" style="width: 20%;float: left;background-color: #f6f4f0;border-radius: 10px">
+        <p class="navigationClass">css教程222</p><hr>
+        <p class="navigationClass">css教程222</p><hr>
+        <p class="navigationClass">css教程222</p><hr>
+        <p class="navigationClass">css教程222</p><hr>
+        <p class="navigationClass">css教程222</p><hr>
+        <p class="navigationClass">css教程222</p><hr>
+      </div>
+      <div style="clear: both"></div>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -11,5 +120,19 @@
 </script>
 
 <style scoped>
-
+  span{
+    font-size: 15px;
+    margin: 0 0 0 2%;
+    cursor: pointer;
+    color: white
+  }
+  hr{
+    background-color: rgba(242,21,255,0.05);border: none;height: 1px
+  }
+  .navigationClass{
+    font-size: 15px;text-align: center;color: #000;cursor: pointer
+  }
+  .menuClass{
+    font-size: 15px;cursor: pointer;
+  }
 </style>
