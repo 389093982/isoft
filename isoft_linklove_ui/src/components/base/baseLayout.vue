f13495d2c5525afc497d331dd8d506536333ccc1
zhoupeng <1875112921@qq.com>
2020-02-02 19:24:56 +0800

    link love 设置字体颜色

--- isoft_linklove_ui/src/components/base/baseLayout.vue
+++ isoft_linklove_ui/src/components/base/baseLayout.vue
@@ -19,7 +19,7 @@
 
     <div id="headShowId">
       <div id="tagPicture" style="width: 30%;float: left;">
-        <h1 style="color: #ff6900">link love</h1>
+        <h1 style="color: #ff6900"><em>link</em> <em style="color: #ff537c">love</em></h1>
       </div>
       <div style="width: 70%;float: left;">
         <input id="searchId" placeholder="搜索..."/>
