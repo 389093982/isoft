a70d00087b849c10c8a4ab9bea3fa146f1d371b2
389093982 <389093982@qq.com>
2020-02-08 16:49:41 +0800

    [Author:z001 - Reason:优化]

--- isoft_linkknown_ui/src/components/Footer/Footer.vue
+++ isoft_linkknown_ui/src/components/Footer/Footer.vue
@@ -10,41 +10,29 @@
 
     <Row class="isoft_bg_white isoft_pd10" style="margin-top: 8px;">
       <Col span="4" style="padding: 50px;">
-        <IVerticalList :showRightBorder="true" title="新手帮助">
-          <IVerticalListItem>新手引导</IVerticalListItem>
-          <IVerticalListItem>视频发布流程</IVerticalListItem>
-          <IVerticalListItem>账号注册</IVerticalListItem>
-        </IVerticalList>
+        <dl>
+          <dd>新手帮助</dd>
+          <dt @click="$router.push({path:'/user/guide'})">新手引导</dt>
+        </dl>
       </Col>
       <Col span="4" style="padding: 50px;">
-        <IVerticalList :showRightBorder="true" title="安全中心">
-          <IVerticalListItem>公司简介</IVerticalListItem>
-          <IVerticalListItem>站点地图</IVerticalListItem>
-        </IVerticalList>
+        <dl>
+          <dd>网站简介</dd>
+          <dt @click="$router.push({path:'/site/index'})">站点地图</dt>
+        </dl>
       </Col>
       <Col span="4" style="padding: 50px;">
-        <IVerticalList :showRightBorder="true" title="安全中心">
-          <IVerticalListItem>公司简介</IVerticalListItem>
-          <IVerticalListItem>站点地图</IVerticalListItem>
-        </IVerticalList>
-      </Col>
-      <Col span="4" style="padding: 50px;">
-        <IVerticalList :showRightBorder="true" title="安全中心">
-          <IVerticalListItem>关于我们</IVerticalListItem>
-          <IVerticalListItem>联系我们</IVerticalListItem>
-          <IVerticalListItem>人才招聘</IVerticalListItem>
-          <IVerticalListItem>商家入驻</IVerticalListItem>
-          <IVerticalListItem>广告服务</IVerticalListItem>
-          <IVerticalListItem>友情链接</IVerticalListItem>
-          <IVerticalListItem>Isoft论坛</IVerticalListItem>
-        </IVerticalList>
+        <dl>
+          <dd>关于我们</dd>
+          <dt @click="$router.push({path:'/job/jobList'})">人才招聘</dt>
+          <dt @click="$router.push({path:'/advertisement/apply'})">广告服务</dt>
+        </dl>
       </Col>
       <Col span="8" style="padding: 50px;">
-        <IVerticalList title="客服中心">
-          <IVerticalListItem>在线客服</IVerticalListItem>
-          <IVerticalListItem>我要举报</IVerticalListItem>
-          <IVerticalListItem>提出意见</IVerticalListItem>
-        </IVerticalList>
+        <dl>
+          <dd>客服中心</dd>
+          <dt @click="$router.push({path:'/ilearning/advise'})">提出意见</dt>
+        </dl>
       </Col>
     </Row>
 
@@ -55,19 +43,28 @@
 </template>
 
 <script>
-  import IVerticalList from "../Common/list/IVerticalList"
-  import IVerticalListItem from "../Common/list/IVerticalListItem"
   import FrindLink from "./FrindLink"
 
   export default {
     name: "Footer",
-    components: {IVerticalList, IVerticalListItem, FrindLink},
+    components: {FrindLink},
     methods: {}
   }
 </script>
 
 <style scoped>
+  dd {
+    font-size: 16px;
+    font-weight: bold;
+    height: 26px;
+    line-height: 26px;
+  }
 
+  dt {
+    font-size: 14px;
+    color: #666;
+    cursor: pointer;
+  }
 
   .top {
     padding: 10px;
