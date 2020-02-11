48c163a5b56a6977f3c4784246b104c3b65c5500
389093982 <389093982@qq.com>
2020-02-11 11:32:51 +0800

    [Author:z001 - Reason:优化]

--- isoft_linkknown_ui/src/components/ILearning/WarmHeartRecommend.vue
+++ isoft_linkknown_ui/src/components/ILearning/WarmHeartRecommend.vue
@@ -11,10 +11,10 @@
         </video>
       </Col>
       <Col span="12" style="padding-left: 10px;">
-        <div v-if="custom_tags && custom_tags.length > 0"
-             style="height: 40px;padding:7px;margin-bottom:10px;background-color: rgba(228,228,228,0.4);">
-          <a class="hovered hvr-grow hoverLinkColor mr5" v-for="(tag, index) in custom_tags"
-             @click="getCustomTagCourses(tag.custom_tag)">{{tag.custom_tag}}</a>
+        <div style="height: 40px;padding:7px;margin-bottom:10px;background-color: rgba(228,228,228,0.4);">
+          <a class="hovered hvr-grow hoverLinkColor mr5" @click="refreshCustomTagCourse('hot')">热门</a>
+          <a class="hovered hvr-grow hoverLinkColor mr5" @click="refreshCustomTagCourse('special')">特色</a>
+          <a class="hovered hvr-grow hoverLinkColor mr5" @click="refreshCustomTagCourse('high_comment')">高评</a>
         </div>
         <div style="column-count:3;">
           <HoverBigImg v-for="(course, index) in display_courses"
@@ -37,29 +37,20 @@
     components: {HoverBigImg},
     data() {
       return {
-        custom_tags: [],
-        custom_tag_courses: [],
         display_courses: [],
       }
     },
     methods: {
-      getCustomTagCourses: function (custom_tag) {
-        // 九宫格
-        this.display_courses = this.custom_tag_courses.filter(course => course.custom_tag == custom_tag).slice(0, 9);
-      },
-      refreshCustomTagCourse: async function () {
-        const result = await QueryCustomTagCourse();
+      refreshCustomTagCourse: async function (custom_tag) {
+        const result = await QueryCustomTagCourse({custom_tag: custom_tag});
         if (result.status == "SUCCESS") {
-          this.custom_tags = result.custom_tags;
-          this.custom_tag_courses = result.custom_tag_courses;
-          if (this.custom_tags && this.custom_tags.length > 0) {
-            this.getCustomTagCourses(this.custom_tags[0].custom_tag);
-          }
+          // 九宫格
+          this.display_courses = result.custom_tag_courses.slice(0, 9);
         }
       }
     },
     mounted() {
-      this.refreshCustomTagCourse();
+      this.refreshCustomTagCourse('hot');
     }
   }
 </script>
