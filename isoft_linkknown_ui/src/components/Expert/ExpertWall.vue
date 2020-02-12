ec3d6d2e3974ed05f0ac3c81c87afd8396922ef6
389093982 <389093982@qq.com>
2020-02-12 17:50:12 +0800

    [Author:z001 - Reason:优化]

--- isoft_linkknown_ui/src/components/Expert/ExpertWall.vue
+++ isoft_linkknown_ui/src/components/Expert/ExpertWall.vue
@@ -21,6 +21,17 @@
         </div>
       </IBeautifulCard>
     </div>
+
+    <div class="isoft_bg_white isoft_pd10 isoft_top5">
+      <IBeautifulCard title="提问达人">
+        <div slot="content">
+          <div v-for="(user,index) in users3">
+            {{user.user_name}}
+            累计提问次数：{{user.question_numbers}}
+          </div>
+        </div>
+      </IBeautifulCard>
+    </div>
   </div>
 </template>
 
@@ -35,6 +46,7 @@
       return {
         users: [],        // 回答总数排行榜
         users2: [],       // 好评排行榜
+        users3: [],       // 提问达人排行榜
       }
     },
     methods: {
@@ -43,6 +55,7 @@
         if (result.status == "SUCCESS") {
           this.users = result.users;
           this.users2 = result.users2;
+          this.users3 = result.users3;
         }
       }
     },
