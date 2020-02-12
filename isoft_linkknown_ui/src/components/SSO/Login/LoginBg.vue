a8164f8eeeea114011972f675b1b15355c7356de
zhoupeng <1875112921@qq.com>
2020-02-12 16:31:45 +0800

    去除“切换账号”

--- isoft_linkknown_ui/src/components/SSO/Login/LoginBg.vue
+++ isoft_linkknown_ui/src/components/SSO/Login/LoginBg.vue
@@ -17,7 +17,7 @@
 
 <style scoped>
   .login_bg {
-    height: 460px;
+    height: 450px;
     width: 100%;
     background: url(../../../assets/sso/login_bg03.jpg) 0px 0px no-repeat;
     background-size: 100%;
