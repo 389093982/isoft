3385b05ea0de44a3220b4b9647d4f99fce23794d
zhoupeng <1875112921@qq.com>
2020-02-18 13:45:59 +0800

    发送验证码优化

--- isoft_iwork_web/core/iworkplugin/node/mail/mail_test.go
+++ isoft_iwork_web/core/iworkplugin/node/mail/mail_test.go
@@ -18,10 +18,10 @@ func Test_Log(t *testing.T) {
 		"1875112921@qq.com",
 	}
 	//邮件主题为"Hello"
-	subject := "Hello"
+	subject := "111111111111"
 	// 邮件正文
-	body := "领创科技专门测试邮件！！！"
-	err := SendMail(mailConn, "lc keji", subject, body, mailTo)
+	body := "1111"
+	err := SendMail(mailConn, "111111111111", subject, body, mailTo)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
