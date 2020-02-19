65c381a25a1d9a339e6cfa84d081ded7acb48e2b
389093982 <389093982@qq.com>
2020-02-19 13:52:47 +0800

    [Author:z001 - Reason:配置优化]

--- isoft_iwork_web/startup/sysconfig/config.go
+++ isoft_iwork_web/startup/sysconfig/config.go
@@ -3,6 +3,7 @@ package sysconfig
 import (
 	"github.com/astaxie/beego"
 	"isoft/isoft_iwork_web/core/iworkutil/fileutil"
+	"strings"
 )
 
 var (
@@ -10,6 +11,7 @@ var (
 	SYSCONFIG_LOGWIRTER_ENABLE bool
 	IWORK_PANICTRACE_SIZE      int
 	ENV_ONUSE                  string
+	PERSISTENT_DIR             string // iwork 框架持久化文件所在目录
 )
 
 func init() {
@@ -19,4 +21,5 @@ func init() {
 	SYSCONFIG_LOGWIRTER_ENABLE = beego.AppConfig.DefaultBool("iwork.logwriter.enable", false)
 	IWORK_PANICTRACE_SIZE, _ = beego.AppConfig.Int("iwork.panicTrace.size")
 	ENV_ONUSE = beego.AppConfig.String("iwork.envname.onuse")
+	PERSISTENT_DIR = strings.TrimSpace(beego.AppConfig.String("iwork.persistent.dir"))
 }
