5ef0b44f332b31c179c788faee07b8290796be9f
389093982 <389093982@qq.com>
2020-02-20 14:53:07 +0800

    [Author:z001 - Reason:广告模块样式优化]

--- isoft_linkknown_ui/src/components/Advertisement/Manage.vue
+++ isoft_linkknown_ui/src/components/Advertisement/Manage.vue
@@ -11,31 +11,34 @@
           <IBeautifulCard title="我的广告清单">
             <div slot="content" style="padding: 10px;">
               <div v-if="advertisements && advertisements.length > 0">
-                <div v-for="(advertisement,index) in advertisements"
-                     style="border-bottom: 1px solid #eee;padding: 10px 0;">
-                  <p>广告显示名称:
-                    <a @click="$router.push({path:'/advertisement/accesslog',query:{id:advertisement.id}})">
-                      {{advertisement.advertisement_label}}
-                    </a>
-                  </p>
-                  <p>链接类型: {{advertisement.linked_type}}</p>
-                  <p>链接地址: {{advertisement.linked_refer}}</p>
-                  <p>显示图片:
-                    <img :src="advertisement.linked_img" style="width: 150px;height: 100px;"/>
-                  </p>
-                  <p>联系人：{{loginUserName}}</p>
-                  <p>广告状态:
-                    <span v-if="advertisement.is_valid === 1">已生效</span>
-                    <span v-else>等待审核</span>
-                  </p>
-                  <p>
-                    <a @click="editAdvertisement(advertisement.id)">编辑</a>
-                    <a @click="$router.push({path:'/advertisement/accesslog',query:{id:advertisement.id}})"
-                       style="color: green;">
-                      查看访问记录
-                    </a>
-                  </p>
-                </div>
+                <Carousel>
+                  <CarouselItem v-for="(advertisement,index) in advertisements">
+                    <div style="padding: 40px 30px;">
+                      <p>广告显示名称:
+                        <a @click="$router.push({path:'/advertisement/accesslog',query:{id:advertisement.id}})">
+                          {{advertisement.advertisement_label}}
+                        </a>
+                      </p>
+                      <p>链接类型: {{advertisement.linked_type}}</p>
+                      <p>链接地址: {{advertisement.linked_refer}}</p>
+                      <p>显示图片:
+                        <img :src="advertisement.linked_img" style="width: 150px;height: 100px;"/>
+                      </p>
+                      <p>联系人：{{loginUserName}}</p>
+                      <p>广告状态:
+                        <span v-if="advertisement.is_valid === 1">已生效</span>
+                        <span v-else>等待审核</span>
+                      </p>
+                      <p>
+                        <a @click="editAdvertisement(advertisement.id)">编辑</a>
+                        <a @click="$router.push({path:'/advertisement/accesslog',query:{id:advertisement.id}})"
+                           style="color: green;">
+                          查看访问记录
+                        </a>
+                      </p>
+                    </div>
+                  </CarouselItem>
+                </Carousel>
               </div>
             </div>
           </IBeautifulCard>
@@ -67,7 +70,7 @@
       refreshPersonalAdvertisement: async function () {
         const result = await GetPersonalAdvertisement();
         if (result.status == "SUCCESS") {
-          this.advertisements = result.advertisements;
+          this.advertisements = result.advertisements.slice(0, 10);
         } else {
           this.$Message.error(result.errorMsg);
         }
