3349f3c12458b310f4c09574541a02fdf131a131
389093982 <389093982@qq.com>
2020-02-20 16:42:00 +0800

    [Author:z001 - Reason:书单阅读模块优化]

--- isoft_linkknown_ui/src/components/IBook/BookArticleDetail.vue
+++ isoft_linkknown_ui/src/components/IBook/BookArticleDetail.vue
@@ -6,29 +6,27 @@
         <div>
           <Button @click="$router.push({path:'/ibook/book_list'})">全部书单</Button>
           <Scroll height="430" style="margin-top: 5px;">
-            <div v-for="bookCatalog in bookCatalogs" class="isoft_hover_red isoft_inline_ellipsis">
+            <div v-for="(bookCatalog, index) in bookCatalogs" class="isoft_hover_red isoft_inline_ellipsis">
               <Icon type="ios-paper-outline"/>
-              <span @click="showDetail(bookCatalog.id)">
-                {{bookCatalog.catalog_name}}
-              </span>
+              <span @click="showDetail(bookCatalog.id, index)" :style="{color: viewIndex === index ? 'red':''}">{{bookCatalog.catalog_name}}</span>
             </div>
           </Scroll>
         </div>
       </Col>
       <Col span="18"
            style="background-color: #fff;border: 1px solid #e6e6e6;border-radius: 4px;padding: 20px;min-height: 500px;">
-        <div style="text-align: right;">
-          <IBeautifulLink>上一篇</IBeautifulLink>
-          <IBeautifulLink>下一篇</IBeautifulLink>
+        <div style="text-align: right;border-bottom: 2px solid #bababa;margin: 0 0 10px 0;padding: 10px 0;">
+          <IBeautifulLink v-if="viewIndex > 0">上一篇</IBeautifulLink>
+          <IBeautifulLink v-if="bookCatalogs && viewIndex < bookCatalogs.length - 1">下一篇</IBeautifulLink>
         </div>
 
         <div style="min-height: 400px;">
           <IShowMarkdown v-if="bookArticle && bookArticle.content" :content="bookArticle.content"/>
         </div>
 
-        <div style="text-align: right;">
-          <IBeautifulLink>上一篇</IBeautifulLink>
-          <IBeautifulLink>下一篇</IBeautifulLink>
+        <div style="text-align: right;border-top: 2px solid #bababa;margin: 0 0 10px 0;padding: 10px 0;">
+          <IBeautifulLink v-if="viewIndex > 0">上一篇</IBeautifulLink>
+          <IBeautifulLink v-if="bookCatalogs && viewIndex < bookCatalogs.length - 1">下一篇</IBeautifulLink>
         </div>
 
         <div style="margin-top: 20px;">
@@ -57,10 +55,12 @@
         bookArticles: [],
         bookArticle: null,
         bookCatalogs: [],
+        viewIndex: 0,
       }
     },
     methods: {
-      showDetail: async function (book_catalog_id) {
+      showDetail: async function (book_catalog_id, index) {
+        this.viewIndex = index;
         const result = await ShowBookArticleDetail(book_catalog_id);
         if (result.status == "SUCCESS") {
           if (result.bookArticle != null) {
@@ -83,6 +83,9 @@
         const result = await BookCatalogList({book_id: book_id});
         if (result.status == "SUCCESS") {
           this.bookCatalogs = result.bookCatalogs;
+          if (this.bookCatalogs && this.bookCatalogs.length > 0) {
+            this.showDetail(this.bookCatalogs[0].id, 0); // 默认显示第一条
+          }
         }
       },
     },
