6773a8d83af2f19f6b494e49456ebf6215c30f6d
zhoupeng <1875112921@qq.com>
2020-02-24 09:14:33 +0800

    5个热门布局整改，上二下三模式。

--- isoft_linkknown_ui/src/components/ILearning/Index.vue
+++ isoft_linkknown_ui/src/components/ILearning/Index.vue
@@ -14,39 +14,46 @@
       <div style="clear: both"></div>
     </div>
 
-    <!--精品课程相关-->
     <!--为您精选、热门、特色、高评-->
     <div style="margin: 40px 0 0 0 ">
       <WarmHeartRecommend/>
     </div>
 
-    <!--话题博客相关-->
-    <!--热门博客-->
-    <div class="isoft_bg_white isoft_pd10 isoft_top5">
-      <BlogRank/>
-    </div>
 
-    <!--热门书单相关-->
-    <!--热门书单-->
-    <div>
-      <BookRank class="isoft_bg_white isoft_pd10"/>
+    <!--5个热门相关做成上2下3的布局-->
+    <div style="float: left;width: 70%">
+      <!--热门博客-->
+      <div class="isoft_bg_white isoft_pd10 isoft_top5">
+        <BlogRank/>
+      </div>
     </div>
-
-    <!--热门资源相关-->
-    <div>
+    <div style="float: right;width: 30%">
       <!--热门应用-->
       <div class="isoft_bg_white isoft_pd10 isoft_top5">
         <ToolBox :placement_name="GLOBAL.placement_host_app_recommand"/>
       </div>
+    </div>
+    <div style="clear: both"></div>
+
+    <div style="float: left;width: 33%">
+      <!--热门书单-->
+      <div>
+        <BookRank class="isoft_bg_white isoft_pd10"/>
+      </div>
+    </div>
+    <div style="float: left;width: 34%">
       <!--热门产品-->
       <div class="isoft_bg_white isoft_pd10" style="margin-top: 5px;">
         <IBeautifulTabLink/>
       </div>
+    </div>
+    <div style="float: left;width: 33%">
       <!-- 热门项目推荐 -->
       <div class="isoft_bg_white isoft_pd10" style="margin-top: 5px;">
         <IHotRecommand :placement_name="GLOBAL.placement_hot_project_recommod"/>
       </div>
     </div>
+    <div style="clear: both"></div>
 
     <!--展示模块  目前是4个展示-->
     <show-modulars></show-modulars>
