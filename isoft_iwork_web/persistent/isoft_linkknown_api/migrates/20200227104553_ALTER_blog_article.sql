5b82771945c2dbba96c8af84c6e6eaedb7b02e7d
zhoupeng <1875112921@qq.com>
2020-02-27 11:00:50 +0800

    补提交 博客删除描述字段 sql文件

---
+++ isoft_iwork_web/persistent/isoft_linkknown_api/migrates/20200227104553_ALTER_blog_article.sql
@@ -0,0 +1,12 @@
+<SqlMigrate>
+	<Id>88</Id>
+	<AppId>5</AppId>
+	<MigrateName>20200225105420_ALTER_blog_article.sql</MigrateName>
+	<MigrateSql>alter table blog_article drop column short_desc;</MigrateSql>
+	<MigrateHash>kzbiJ/h8iuOQjBC+dxzaYCaylA1AOeQENp/b8asZsg8=</MigrateHash>
+	<Effective>true</Effective>
+	<CreatedBy>SYSTEM</CreatedBy>
+	<CreatedTime>2020-02-25T10:54:21+08:00</CreatedTime>
+	<LastUpdatedBy>SYSTEM</LastUpdatedBy>
+	<LastUpdatedTime>2020-02-25T10:54:21+08:00</LastUpdatedTime>
+</SqlMigrate>
