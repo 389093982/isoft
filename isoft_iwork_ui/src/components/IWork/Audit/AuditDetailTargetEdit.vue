52d160e883eb5f480d02ce21abe480e60d87bf66
389093982 <389093982@qq.com>
2020-03-01 11:28:29 +0800

    [Author:z001 - Reason: 内容审核系统优化]

--- isoft_iwork_ui/src/components/IWork/Audit/AuditDetailTargetEdit.vue
+++ isoft_iwork_ui/src/components/IWork/Audit/AuditDetailTargetEdit.vue
@@ -4,7 +4,8 @@
       <TabPane v-for="(item, index) in update_cases" :label="item.case_name ? item.case_name : '场景 ' + (index + 1)" tab="tab_level_2">
 
         场景名称: <span style="color: #00ce00;">参考案例：生效、失效、审核通过、内容不合法等中文或英文</span>
-        <span style="margin-left: 100px;">删除按钮颜色设置：<ColorPicker v-model="item.case_color" size="small" alpha recommend/></span>
+        <span style="margin-left: 100px;">按钮颜色设置：<ColorPicker v-model="item.case_color" size="small" alpha
+                                                              recommend/></span>
         <Button type="error" size="small" @click="handleRemove(index)">删除</Button>
 
         <Input type="text" v-model="item.case_name" placeholder="请输入场景名称" style="margin: 5px 0;"></Input>
