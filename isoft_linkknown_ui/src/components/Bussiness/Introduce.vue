945974423386d3577f3c967a7f654e6fd82938f8
389093982 <389093982@qq.com>
2020-03-05 14:05:33 +0800

    [Author:z001 - Reason: 商业合作优化]

--- isoft_linkknown_ui/src/components/Bussiness/Introduce.vue
+++ isoft_linkknown_ui/src/components/Bussiness/Introduce.vue
@@ -1,6 +1,160 @@
 <template>
   <div>
-    AAAAAAAAAAAAAAA
+    <div class="bg" style="color: white;">
+      <div style="padding: 40px 60px;">
+        <div style="float: left;font-size: 20px;color: #ff6400;">
+          链知网&nbsp;|&nbsp;<span>商家入驻</span>
+        </div>
+        <div class="hoverBottomBorder" style="float: right;">
+          招商中心
+        </div>
+      </div>
+
+      <p style="font-size: 50px;line-height: 50px;margin-top: 115px;text-align: center;">
+        加入我们，展现最真实的自我
+      </p>
+
+      <p style="font-size: 20px;line-height: 40px;text-align: center;margin-top: 50px;">
+        网站开发、广告推广、素材设计、商品服务，发现更多精彩，期待您的入驻 全部类别
+      </p>
+
+      <div style="text-align: center;margin-top: 50px;">
+        <span style="font-size: 18px;border: 2px solid white;padding: 10px 30px;">免费入驻</span>
+      </div>
+    </div>
+
+    <div class="business isoft_bg_white" style="padding: 30px 200px;display: flex;text-align: center;">
+      <div style="width: 300px;">
+        <img style="border-radius: 50%;width: 65px;height: 65px;margin: 20px;" src="../../assets/default.png"/>
+        <p class="text2">注册链知网账号</p>
+      </div>
+      <div style="width: 300px;">
+        <img style="border-radius: 50%;width: 65px;height: 65px;margin: 20px;" src="../../assets/default.png"/>
+        <p class="text2">认证并获取资格</p>
+      </div>
+      <div style="width: 300px;">
+        <img style="border-radius: 50%;width: 65px;height: 65px;margin: 20px;" src="../../assets/default.png"/>
+        <p class="text2">发布您的作品或需求赚钱</p>
+      </div>
+    </div>
+
+    <div style="padding: 20px 100px 30px 100px;margin: 0 auto;">
+      <div style="font-size: 30px;text-align: center;">
+        优秀合作伙伴
+      </div>
+
+      <div class="isoft_bg_white" style="margin: 80px;padding: 25px;min-height: 250px;display: flex;">
+        <div style="width: 20%;">
+          <img style="width: 80%;height: 200px;" src="../../assets/default.png"/>
+        </div>
+        <div style="width: 80%;display: flex;">
+          <div style="width: 50%;">
+            <div style="margin-bottom: 20px;">
+              <span style="font-size: 22px;">周子聪</span>&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>泽凯品牌创始人</span>&nbsp;&nbsp;|&nbsp;&nbsp;
+              <span>28岁</span>
+            </div>
+
+            <div style="    font-size: 14px;
+    color: #666;
+    padding-left: 15px;
+    line-height: 40px;
+    height: 40px;
+    background: #f0f0f0;
+    font-weight: 700;">入驻前：
+            </div>
+
+            <div style="margin: 10px 0;">
+              <p style="color: #999;"><span style="color: black;">身份：</span>东华理工大学大二学生</p>
+              <p style="color: #999;"><span style="color: black;">收入：</span>家里400元/月生活费</p>
+              <p style="color: #999;"><span style="color: black;">境况：</span>渴望独立赚钱，家里却反对创业</p>
+            </div>
+          </div>
+          <div style="width: 50%;">
+            <div style="margin-bottom: 20px;">
+              <span style="font-size: 18px;">擅长</span>
+              <span class="tag">平面设计</span>
+              <span class="tag">营销策划</span>
+            </div>
+
+            <div style="    font-size: 14px;
+    color: #666;
+    padding-left: 15px;
+    line-height: 40px;
+    height: 40px;
+    background: #eef6ff;
+    font-weight: 700;">入驻后：
+            </div>
+            <div style="margin: 10px 0;">
+              <p style="color: #999;"><span style="color: black;">身份：</span><span style="color: blue;">TOP10服务商</span>
+              </p>
+              <p style="color: #999;"><span style="color: black;">收入：</span><span
+                style="color: #ff4f0d;">家里400元/月生活费</span></p>
+              <p style="color: #999;"><span style="color: black;">境况：</span>入驻12年，服务客户21876个，100%好评率，实现了从个人创业到集团化经营的飞跃
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="isoft_bg_white" style="margin: 80px;padding: 25px;min-height: 250px;display: flex;">
+        <div style="width: 20%;">
+          <img style="width: 80%;height: 200px;" src="../../assets/default.png"/>
+        </div>
+        <div style="width: 80%;display: flex;">
+          <div style="width: 50%;">
+            <div style="margin-bottom: 20px;">
+              <span style="font-size: 22px;">周子聪</span>&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>泽凯品牌创始人</span>&nbsp;&nbsp;|&nbsp;&nbsp;
+              <span>28岁</span>
+            </div>
+
+            <div style="    font-size: 14px;
+    color: #666;
+    padding-left: 15px;
+    line-height: 40px;
+    height: 40px;
+    background: #f0f0f0;
+    font-weight: 700;">入驻前：
+            </div>
+
+            <div style="margin: 10px 0;">
+              <p style="color: #999;"><span style="color: black;">身份：</span>东华理工大学大二学生</p>
+              <p style="color: #999;"><span style="color: black;">收入：</span>家里400元/月生活费</p>
+              <p style="color: #999;"><span style="color: black;">境况：</span>渴望独立赚钱，家里却反对创业</p>
+            </div>
+          </div>
+          <div style="width: 50%;">
+            <div style="margin-bottom: 20px;">
+              <span style="font-size: 18px;">擅长</span>
+              <span class="tag">平面设计</span>
+              <span class="tag">营销策划</span>
+            </div>
+
+            <div style="    font-size: 14px;
+    color: #666;
+    padding-left: 15px;
+    line-height: 40px;
+    height: 40px;
+    background: #eef6ff;
+    font-weight: 700;">入驻后：
+            </div>
+            <div style="margin: 10px 0;">
+              <p style="color: #999;"><span style="color: black;">身份：</span><span style="color: blue;">TOP10服务商</span>
+              </p>
+              <p style="color: #999;"><span style="color: black;">收入：</span><span
+                style="color: #ff4f0d;">家里400元/月生活费</span></p>
+              <p style="color: #999;"><span style="color: black;">境况：</span>入驻12年，服务客户21876个，100%好评率，实现了从个人创业到集团化经营的飞跃
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style="font-size: 30px;text-align: center;">孵化公司超过 10 万家，他们的故事都很精彩</div>
+      <div class="isoft_button_blue" style="width: 300px;margin: 20px auto;">查看更多创业故事</div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +165,32 @@
 </script>
 
 <style scoped>
+  .bg {
+    background: url("../../../static/images/business/bg.jpg") no-repeat;
+    background-size: 100% 100%;
+    height: 450px;
+  }
 
+  .hoverBottomBorder:hover {
+    cursor: pointer;
+    border-bottom: 1px solid white;
+  }
+
+  .business .text2 {
+    font-size: 22px;
+    line-height: 100px;
+  }
+
+  .tag {
+    display: inline-block;
+    background-color: #f0f0f0;
+    border-radius: 3px;
+    height: 24px;
+    line-height: 24px;
+    font-size: 12px;
+    color: #333;
+    text-align: center;
+    padding: 0 5px;
+    font-weight: 100;
+  }
 </style>
