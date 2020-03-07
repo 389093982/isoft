263abc24c7fa14f4fcdbca75fdecc4e0eb2a22da
zhoupeng <1875112921@qq.com>
2020-03-07 13:24:59 +0800

    book表增加comments字段，记录评论次数

---
+++ isoft_iwork_web/persistent/isoft_linkknown_api/migrates/20200307130555_ALTER_book.sql
@@ -0,0 +1,12 @@
+<SqlMigrate>
+	<Id>97</Id>
+	<AppId>5</AppId>
+	<MigrateName>20200307130555_ALTER_book.sql</MigrateName>
+	<MigrateSql>alter table book add column comments int(11) NOT NULL DEFAULT 0;</MigrateSql>
+	<MigrateHash>M4ekKRZpx4pxQcrFASjj9PJo25kIKEIbqTHSPtoJ97c=</MigrateHash>
+	<Effective>true</Effective>
+	<CreatedBy>SYSTEM</CreatedBy>
+	<CreatedTime>2020-03-07T13:05:55+08:00</CreatedTime>
+	<LastUpdatedBy>SYSTEM</LastUpdatedBy>
+	<LastUpdatedTime>2020-03-07T13:05:55+08:00</LastUpdatedTime>
+</SqlMigrate>
