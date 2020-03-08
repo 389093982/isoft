d2b4a732234c230b72a50ab719f143a5114e985a
389093982 <389093982@qq.com>
2020-03-08 17:55:19 +0800

    [Author:z001 - Reason: 商品模块优化]

--- isoft_linkknown_ui/src/components/Business/BusinessDetail2.vue
+++ isoft_linkknown_ui/src/components/Business/BusinessDetail2.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    保障服务|用户评价|店铺信息
+    <Tab :tab-counts="3" style="margin-top: 30px;">
+      <div slot="titlePane1">保障服务</div>
+      <div slot="titlePane2">用户评价</div>
+      <div slot="titlePane3">店铺信息</div>
+
+      <div slot="tabPane1">tabPane1</div>
+      <div slot="tabPane2">tabPane2</div>
+      <div slot="tabPane3">tabPane3</div>
+    </Tab>
   </div>
 </template>
 
 <script>
+  import Tab from "./Tab";
+
   export default {
-    name: "BusinessDetail2"
+    name: "BusinessDetail2",
+    components: {Tab}
   }
 </script>
 
