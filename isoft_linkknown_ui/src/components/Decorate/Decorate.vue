ec119aa850bffbe6017fe1708f2dac9c57df069d
389093982 <389093982@qq.com>
2020-03-09 19:00:06 +0800

    [Author:z001 - Reason: 装饰系统设计]

--- isoft_linkknown_ui/src/components/Decorate/Decorate.vue
+++ isoft_linkknown_ui/src/components/Decorate/Decorate.vue
@@ -1,15 +1,21 @@
 <template>
-  <div style="border: 1px solid #eee;margin: 50px;padding: 50px;">
-    <div style="display: flex;">
-      <div style="width: 60%;">
-        <span v-if="decorates" v-for="(_decorate, index) in decorates" @click="decorate = _decorate">{{_decorate.decorate_name}}</span>
-      </div>
-      <div style="width: 40%;text-align: right;" class="isoft_hover_red" @click="editDecorate">
-        <Icon type="md-add"/>
-        快速新建装修位
-      </div>
+  <div style="border: 1px solid #eee;margin: 50px;">
+    <div style="border-bottom: 1px solid #e5e5e5;background: #f9f9f9;">
+      <ul class="clear">
+        <li v-if="decorates" v-for="(_decorate, index) in decorates"
+            :class="decorate && decorate.id === _decorate.id ? 'active': ''" @click="decorate = _decorate">
+          {{_decorate.decorate_name}}
+        </li>
+        <li style="float: right;" @click="editDecorate">
+          <Icon type="md-add"/>
+          快速新建装修位
+        </li>
+      </ul>
     </div>
-    <DecorateItems ref="decorateItems" v-if="decorate" :decorate="decorate"/>
+
+    <div style="padding: 20px 100px 20px 20px;">
+      <DecorateItems ref="decorateItems" v-if="decorate" :decorate="decorate"/>
+    </div>
   </div>
 </template>
 
@@ -32,12 +38,17 @@
     },
     data() {
       return {
-        decorates: null,  // 所有装修位
+        decMaxLen: 5,  // 装修位最大数量
+        decorates: null,   // 所有装修位
         decorate: null,    // 当前正在编辑的装修位
       }
     },
     methods: {
       editDecorate: async function () {
+        if (this.decorates && this.decorates.length >= this.decMaxLen) {
+          this.$Message.error("装修位超过最大数量!");
+          return;
+        }
         let params = {
           'referer_type': this.referer_type,
           'referer_id': this.referer_id,
@@ -53,6 +64,10 @@
         const result = await LoadAllDecorates({referer_type: this.referer_type, referer_id: this.referer_id});
         if (result.status === "SUCCESS") {
           this.decorates = result.decorates;
+          // 初始选中第一个
+          if (this.decorate === null && this.decorates && this.decorates.length > 0) {
+            this.decorate = result.decorates[0];
+          }
         }
       },
     },
@@ -63,5 +78,17 @@
 </script>
 
 <style scoped>
+  ul li {
+    padding: 10px 50px;
+    list-style: none;
+    display: inline-block;
+    cursor: pointer;
+  }
 
+  .active {
+    background-color: #fff;
+    border-top: 3px solid #ff9d4b;
+    border-left: 1px solid #eee;
+    border-right: 1px solid #eee;
+  }
 </style>
--- isoft_linkknown_ui/src/components/Decorate/DecorateItems.vue
+++ isoft_linkknown_ui/src/components/Decorate/DecorateItems.vue
@@ -1,43 +1,41 @@
 <template>
   <div v-if="decorate">
-    <Row :gutter="10" style="text-align: center;">
-      <Col span="8">图片/视频</Col>
-      <Col span="8">文本描述</Col>
-      <Col span="8">链接地址
-        <Icon type="md-add" @click="handleAdd"/>
-      </Col>
-    </Row>
-    <Form ref="formDynamic" :model="formDynamic">
+    <Form ref="formDynamic" :model="formDynamic" v-if="formDynamic.items && formDynamic.items.length > 0">
+      <Row :gutter="10" style="text-align: center;">
+        <Col span="6">图片/视频</Col>
+        <Col span="9">文本描述</Col>
+        <Col span="9">链接地址
+        </Col>
+      </Row>
       <Row :gutter="10" v-for="(item, index) in formDynamic.items" :key="index">
-        <Col span="8">
+        <Col span="6">
           <FormItem :prop="'items.' + index + '.media_path'"
                     :rules="{required: true, message: '图片/视频[' + index +']不能为空！', trigger: 'blur'}">
             <Input v-model.trim="item.media_path" placeholder="请选择图片/视频"></Input>
           </FormItem>
         </Col>
-        <Col span="8">
+        <Col span="9">
           <FormItem :prop="'items.' + index + '.decorate_text'"
                     :rules="{required: true, message: '文本描述[' + index +']不能为空！', trigger: 'blur'}">
-            <Input v-model.trim="item.decorate_text" placeholder="请输入文本描述"></Input>
+            <Input type="textarea" :rows="3" v-model.trim="item.decorate_text" placeholder="请输入文本描述"></Input>
           </FormItem>
         </Col>
-        <Col span="8">
+        <Col span="9">
           <FormItem :prop="'items.' + index + '.link_href'"
                     :rules="{required: true, message: '链接地址[' + index +']不能为空！', trigger: 'blur'}">
             <Input v-model.trim="item.link_href" placeholder="请输入链接地址"></Input>
           </FormItem>
 
-          <div style="position: absolute;top: 3px;right: -30px;">
-            <Icon type="md-close" @click="handleRemove(index)"/>
-            <Icon type="md-add" @click="handleAdd"/>
-            <Icon type="md-checkmark" @click="handleSave(index)"/>
+          <div style="position: absolute;top: 3px;right: -60px;cursor: pointer;">
+            <Icon type="md-close" @click="handleRemove(index)" title="删除"/>
+            <Icon type="md-add" @click="handleAdd" title="增加"/>
+            <Icon type="md-checkmark" @click="handleSave(index)" title="保存"/>
           </div>
         </Col>
       </Row>
-      <FormItem>
-        <Button type="success" size="small" @click="handleSubmit('formDynamic')" style="margin-right: 6px">提交</Button>
-      </FormItem>
     </Form>
+
+    <div v-else @click="handleAdd" class="isoft_button_green1" style="width: 400px;margin: 0 auto;">您还没有装修项，立马创建</div>
   </div>
 </template>
 
@@ -72,15 +70,7 @@
       handleRemove(index) {
         this.formDynamic.items.splice(index, 1);
       },
-      handleSubmit(name) {
-        this.$refs[name].validate(async (valid) => {
-          if (valid) {
-            alert(this.formDynamic.items.length);
-          }
-        })
-      },
       handleAdd() {
-        this.index++;
         this.formDynamic.items.push({
           id: 0,
           media_path: '',
@@ -105,10 +95,13 @@
           'media_path': this.formDynamic.items[index].media_path,
           'decorate_text': this.formDynamic.items[index].decorate_text,
           'link_href': this.formDynamic.items[index].link_href,
-        }
+        };
         const result = await EditDecorateItem(params);
         if (result.status === "SUCCESS") {
+          this.$Message.success("保存成功！");
           this.refreshLoadDecorateData();
+        } else {
+          this.$Message.error(result.errorMsg);
         }
       }
     },
