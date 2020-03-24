6418e724025e08116188b307be243f48255d2522
zhoupeng <1875112921@qq.com>
2020-03-24 22:25:42 +0800

    视频播放

--- isoft_linkknown_ui/src/components/ILearning/Course/VideoPlay.vue
+++ isoft_linkknown_ui/src/components/ILearning/Course/VideoPlay.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="isoft_bg_white isoft_pd10" style="height: 800px;">
-    <Row :gutter="10">
-      <Col span="16">
-        <div v-if="course && curVideo" style="height: 50px;line-height: 50px;font-size: 16px;
-          color: #1c1f21;font-weight: 700;padding: 2px 10px;">
-          {{course.course_name}} / {{curVideo.video_name | filterSuffix }}
+  <div style="background-color: #232325;height: 800px;width: 99.5%">
+    <Row>
+      <Col span="15" style="position: relative;left: 80px;">
+        <div v-if="course && curVideo" style="height: 30px;margin: 10px 0 0 0 ;color: #999">
+          正在播放: {{course.course_name}} / {{curVideo.video_name | filterSuffix }}
         </div>
-        <div style="margin: 0 auto;">
-          <video ref="video" width="100%" height="100%" controls preload="auto" id="videoPath" autoplay="autoplay"
-                 controlslist="nodownload">
+        <div>
+          <video ref="video" width="100%" height="100%" controls preload="auto" id="videoPath" autoplay="autoplay" controlslist="nodownload">
             <source type="video/mp4">
             <source type="video/ogg">
             您的浏览器不支持Video标签。
           </video>
         </div>
       </Col>
-      <Col span="8" style="padding: 16px 20px;">
-        <div class="isoft_auto_with title">
+      <Col span="6" offset="2">
+        <div style="font-size: 20px;margin:35px 0 0 0 ;color: #999">
           推荐课程
         </div>
-        <div style="padding: 10px 0px;border-top: 2px solid #edeff0;">
-          <div class="course_item isoft_inline_ellipsis" v-for="(course, index) in recommendCourses">
-            {{course.course_name}}
+        <div style="padding: 10px 0 0 10px ;">
+          <div class="course_item" v-for="(course, index) in recommendCourses">
+            <div>{{course.course_name}}</div>
+            <div class="course_small_image" style="width: 155px;">
+              <img v-if="course.small_image" :src="course.small_image" height="100" width="155"/>
+              <img v-else src="../../../assets/default.png" height="100" width="155"/>
+              <div class="ico_play"></div>
+            </div>
+
           </div>
         </div>
       </Col>
@@ -38,21 +42,21 @@
       return {
         recommendCourses: [],
         cVideos: [],
-        course: null,
+        course: '',
+        curVideo: '',
         curVideoId: -1, // 当前播放视频 id
-        curVideo: null,
       }
     },
     methods: {
       playVideo: function (video_id) {
         this.curVideoId = video_id;
-        var xhr = new XMLHttpRequest();                                                     //创建XMLHttpRequest对象
+        let xhr = new XMLHttpRequest();                                                     //创建XMLHttpRequest对象
         xhr.open('GET', videoPlayUrl + "?video_id=" + video_id, true);                      //配置请求方式、请求地址以及是否同步
         xhr.responseType = 'blob';                                                          //设置结果类型为blob;
         xhr.onload = function (e) {
           if (this.status === 200) {
             // 获取blob对象
-            var blob = this.response;
+            let blob = this.response;
             // 获取blob对象地址，并把值赋给容器
           document.getElementById("videoPath").setAttribute("src", URL.createObjectURL(blob));
           }
@@ -61,16 +65,16 @@
       },
       refreshCustomTagCourse: async function (custom_tag) {
         const result = await QueryCustomTagCourse({custom_tag: custom_tag});
-        if (result.status == "SUCCESS") {
+        if (result.status === "SUCCESS") {
           this.recommendCourses = result.custom_tag_courses;
         }
       },
       addPlayNextEventListener: function () {
-        var _this = this;
-        var video = document.getElementById("videoPath");
+        let _this = this;
+        let video = document.getElementById("videoPath");
         video.addEventListener("ended", function () {
           let nexts = _this.cVideos.filter(video => video.id > _this.curVideoId);
-          if (nexts != null && nexts != undefined && nexts.length > 0) {
+          if (nexts != null && nexts !== undefined && nexts.length > 0) {
             let video_id = nexts[0].id;
             _this.playVideo(video_id);
           }
@@ -78,8 +82,8 @@
       },
       refreshCorsedetail: async function () {
         const course_id = this.$route.query.course_id;
-        const result = await ShowCourseDetail(course_id);
-        if (result.status == "SUCCESS") {
+        const result = await ShowCourseDetail({course_id:course_id});
+        if (result.status === "SUCCESS") {
           this.course = result.course;
           // 根据 id 顺序排序
           this.cVideos = result.cVideos.sort((video1, video2) => video1.id < video2.id);
@@ -112,28 +116,38 @@
 </script>
 
 <style scoped>
-  .title {
-    font-size: 18px;
-    font-weight: normal;
-    height: 35px;
-    line-height: 35px;
-    font-family: "微软雅黑";
-  }
-
-  .title::after {
-    content: "";
-    display: block;
-    height: 3px;
-    border-bottom: 3px solid red;
+  video {
+    object-fit:fill;
+    width:795px;
+    height:450px;
   }
 
   .course_item {
-    padding: 5px 8px;
+    color: #999;
+    padding: 5px;
     cursor: pointer;
   }
 
   .course_item:hover {
-    background-color: rgba(0, 191, 20, 0.68);
     color: white;
   }
+
+  .course_item:hover .course_small_image{
+    display: block;
+  }
+
+  .course_small_image{
+    display: none;
+  }
+
+  .ico_play {
+    background: url(../../../assets/ico_play.png) no-repeat;
+    background-size: 20px;
+    position: relative;
+    top: -30px;
+    left: 130px;
+    width: 20px;
+    height: 20px;
+  }
+
 </style>
