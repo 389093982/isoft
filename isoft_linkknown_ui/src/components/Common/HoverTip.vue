269e4255543b1a5afc7edc79db6052d9cc15f9f5
389093982 <389093982@qq.com>
2020-04-16 16:10:25 +0800

    [Author:z001 - Reason: 首页优化]

--- isoft_linkknown_ui/src/components/Common/HoverTip.vue
+++ isoft_linkknown_ui/src/components/Common/HoverTip.vue
@@ -1,27 +1,35 @@
 <template>
   <span>
-    <div class="box" style="display: inline-block;color: red;">
+    <div class="box" style="display: inline-block;" @click="showBusiness = true">
       <span style="position: relative;">
-        <!--<img style="width: 120px;height: 150px;position: absolute;top: -90px;left: -20px;" src="../../../static/images/logo01.gif"/>-->
-        <img style="width: 120px;height: 150px;position: absolute;top: -90px;left: -20px;" src="../../../static/images/logo02.gif"/>
+        <img style="width: 120px;height: 150px;position: absolute;top: -90px;left: -20px;" src="../../../static/images/logo01.gif"/>
+        <!--<img style="width: 120px;height: 150px;position: absolute;top: -90px;left: -20px;" src="../../../static/images/logo02.gif"/>-->
       </span>
       <div style="margin: 15px 5px 5px 50px;color: white;text-align: center;" class="isoft_font16">
         <p>创业必备</p>
-        <p>限时免费领</p>
+        <p>点击我了解</p>
         <div style="position: relative;">
           <img style="position: absolute;right: 10px;top: -33px;width: 10px;height: 17px;" src="../../../static/images/right.png"/>
         </div>
       </div>
     </div>
 
-    <div>
+    <div class="center_box" v-if="showBusiness">
+      <span class="isoft_point_cursor" @click="showBusiness = false"
+            style="position: absolute;right: 10px;top: 10px;"><Icon type="md-close" size="20"/></span>
+      <img src="../../../static/images/jianzhi.png" style="width: 280px;height: 450px;"/>
     </div>
   </span>
 </template>
 
 <script>
   export default {
-    name: "HoverTip"
+    name: "HoverTip",
+    data (){
+      return {
+        showBusiness: false,
+      }
+    }
   }
 </script>
 
@@ -42,5 +50,15 @@
   }
   .box:hover {
     left: 0;
+  }
+  .center_box {
+    position: fixed;
+    top: 80px;
+    left: 10%;
+    right: 10%;
+    padding: 18px 20px;
+    background: rgba(248, 248, 248, 1);
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.16);
+    border-radius: 6px;
   }
 </style>
