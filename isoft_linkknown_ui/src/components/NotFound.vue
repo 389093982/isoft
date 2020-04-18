d83cf6381ab692fee0ea50596f0d181c53dbcfa8
389093982 <389093982@qq.com>
2020-04-18 19:40:41 +0800

    [Author:z001 - Reason: 404 頁面]

---
+++ isoft_linkknown_ui/src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="text-align: center;margin-top: 250px;">
+    <h2>抱歉，您输入的地址没有找到</h2>
+    <h3>o(╥﹏╥)o</h3>
+    <button @click="$router.push({path:'/'})">点击返回主页</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NotFound"
+  }
+</script>
+
+<style scoped>
+
+</style>
