10200a1ebaab25ac8141c8c5f5139da9fb799828
389093982 <389093982@qq.com>
2020-04-19 18:49:46 +0800

    [Author:z001 - Reason: github 第三方登录实现初版]

--- isoft_iwork_web/persistent/isoft_linkknown_api/migrates/20200419172352_ALTER_user.sql
+++ isoft_iwork_web/persistent/isoft_linkknown_api/migrates/20200419172352_ALTER_user.sql
@@ -2,11 +2,11 @@
 	<Id>114</Id>
 	<AppId>5</AppId>
 	<MigrateName>20200419172352_ALTER_user.sql</MigrateName>
-	<MigrateSql>alter table user add column third_user_type varchar(50) COLLATE utf8_bin NOT NULL DEFAULT &#39;lk&#39; COMMENT &#39;第三方登录类型&#39;;&#xA;alter table user add column third_user_name varchar(255) COLLATE utf8_bin NOT NULL DEFAULT &#39;&#39; COMMENT &#39;第三方登录用户&#39;;</MigrateSql>
-	<MigrateHash>Yxf0M7Kmhtc0Rme2evq0A8TlXsFbyKQXF3hOz3/TUHA=</MigrateHash>
+	<MigrateSql>alter table user add column third_user_type varchar(50) COLLATE utf8_bin NOT NULL DEFAULT &#39;lk&#39; COMMENT &#39;第三方登录类型&#39;;</MigrateSql>
+	<MigrateHash>26q5NwC/ZHOupJE8w9s3/ZqDNGU1kSnt47e9D3jdCCA=</MigrateHash>
 	<Effective>true</Effective>
 	<CreatedBy>SYSTEM</CreatedBy>
 	<CreatedTime>2020-04-19T17:23:53+08:00</CreatedTime>
 	<LastUpdatedBy>SYSTEM</LastUpdatedBy>
-	<LastUpdatedTime>2020-04-19T17:23:53+08:00</LastUpdatedTime>
+	<LastUpdatedTime>2020-04-19T17:56:04+08:00</LastUpdatedTime>
 </SqlMigrate>
