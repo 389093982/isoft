eb3b64446abab00c7d1ad25e8947f60baa6b6ee6
zhoupeng <1875112921@qq.com>
2020-04-19 10:28:40 +0800

    联系我们，做成组件

--- isoft_linkknown_ui/src/components/Footer/Footer.vue
+++ isoft_linkknown_ui/src/components/Footer/Footer.vue
@@ -10,42 +10,47 @@
     </Row>
 
     <Row class="isoft_bg_white isoft_pd10" style="border-top: 1px solid gainsboro;height: 150px;padding: 40px 0 0 0 ">
-      <Col span="4">
+      <Col span="2">
         &nbsp;
       </Col>
       <Col span="4">
-        <dl>
-          <dd>新手帮助</dd>
-          <dt @click="$router.push({path:'/user/guide'})">新手引导</dt>
-        </dl>
+        <div>
+          <div><b style="font-size: 16px">新手帮助</b></div>
+          <span class="tipHoverOrange" @click="$router.push({path:'/user/guide'})" style="cursor: pointer;font-size: 14px">新手引导</span>
+        </div>
       </Col>
       <Col span="4">
-        <dl>
-          <dd>网站简介</dd>
-          <dt @click="$router.push({path:'/site/index'})">站点地图</dt>
-        </dl>
+        <div>
+          <div><b style="font-size: 16px">网站简介</b></div>
+          <span class="tipHoverOrange" @click="$router.push({path:'/site/index'})" style="cursor: pointer;font-size: 14px">站点地图</span>
+        </div>
       </Col>
       <Col span="4">
-        <dl>
-          <dd>关于我们</dd>
-          <dt @click="$router.push({path:'/job/jobList'})">人才招聘</dt>
-          <dt @click="$router.push({path:'/advertisement/advApply'})">广告服务</dt>
-        </dl>
+        <div>
+          <div><b style="font-size: 16px">关于我们</b></div>
+          <span class="tipHoverOrange" @click="$router.push({path:'/job/jobList'})" style="cursor: pointer;font-size: 14px">人才招聘</span><br>
+          <span class="tipHoverOrange" @click="$router.push({path:'/advertisement/advApply'})" style="cursor: pointer;font-size: 14px">广告服务</span>
+        </div>
       </Col>
       <Col span="4">
-        <dl>
-          <dd>客服中心</dd>
-          <dt @click="$router.push({path:'/ilearning/advise'})">提出意见</dt>
-        </dl>
+        <div>
+          <div><b style="font-size: 16px">客服中心</b></div>
+          <span class="tipHoverOrange" @click="$router.push({path:'/ilearning/advise'})" style="cursor: pointer;font-size: 14px">提出意见</span>
+        </div>
       </Col>
       <Col span="4">
-        &nbsp;
+        <div>
+          <div><b style="font-size: 16px">联系我们</b></div>
+          <contact-us></contact-us>
+        </div>
       </Col>
     </Row>
 
     <Row style="background-color: rgb(229, 229, 229)">
       <div style="width: 100%;height: 100px;text-align: center ;">
-        <p style="margin: 20px;" class="hovered isoft_hover_red2 mr5">Copyright © 2019-{{getNowYear()}} LinkKnown 版权所有</p>
+        <div style="margin-top: 20px;">
+          <span class="hovered isoft_hover_red2 mr5">Copyright © 2019-{{getNowYear()}} LinkKnown 版权所有</span>
+        </div>
         <div style="margin-top: 20px">
           <router-link to="/ilearning/domainName" target="_blank" class="hovered isoft_hover_red2 mr5">域名证书</router-link>&nbsp;
           <a href="http://www.beian.miit.gov.cn/" target="_blank" class="hovered isoft_hover_red2 mr5">ICP备案号:2020627762077</a>
@@ -57,10 +62,11 @@
 
 <script>
   import FrindLink from "./FrindLink"
+  import ContactUs from "../Common/contactUs/contactUs";
 
   export default {
     name: "Footer",
-    components: {FrindLink},
+    components: {ContactUs, FrindLink},
     methods: {
       getNowYear:function () {
         return new Date().getFullYear();
@@ -94,5 +100,8 @@
   #footer {
     position: relative;
     top: -12px;
+  }
+  .tipHoverOrange:hover{
+    color: #ff6900;
   }
 </style>
