5c1f80a8be12ae48bdd23c9f7dd57a477f4f7594
389093982 <389093982@qq.com>
2020-04-24 19:39:28 +0800

    [Author:z001 - Reason: iwork 框架功能完善 - 登录记录表查询 bug 修复]

--- isoft_linkknown_ui/src/components/Common/TagRender.vue
+++ isoft_linkknown_ui/src/components/Common/TagRender.vue
@@ -1,8 +1,6 @@
 <template>
-  <span @mouseleave="showMore = false">
+  <span>
     <span class="isoft_tag3" v-for="(tag, index) in showTags">{{tag | filterLimitFunc}}</span>
-    <span class="isoft_tag3" v-for="(tag, index) in hideTags" :style="{display: showMore ? 'inline-block': 'none'}">{{tag}}</span>
-    <span class="isoft_tag3" v-if="hasMore && !showMore" @mouseenter="showMoreLazy">...</span>
   </span>
 </template>
 
@@ -16,30 +14,16 @@
           return ['测试1', '测试2', '测试3', '测试4', '测试5', '测试6'];
         },
       },
-      showMaxLen: {
-        type: Number,
-        default: 6,
-      }
     },
     data() {
       return {
         showTags: [],
-        hideTags: [],
-        hasMore: false,
-        showMore: false,
       }
     },
     methods: {
-      showMoreLazy: function () {
-        setTimeout(() => {
-          this.showMore = true;
-        }, 300);
-      }
     },
     mounted() {
-      this.hasMore = this.tags.length > 3;
-      this.showTags = this.tags.slice(0, 3);
-      this.hideTags = this.tags.slice(3, this.showMaxLen);
+      this.showTags = this.tags.slice(0, 5);
     },
     filters: {
       // 内容超长则显示部分
