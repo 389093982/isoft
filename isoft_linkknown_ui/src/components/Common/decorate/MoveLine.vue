7d288ae44068a7ad8cc15bc2ffa4cc6b8b89d840
zhoupeng <1875112921@qq.com>
2020-04-26 21:30:55 +0800

    博客中间图，暂时这样了

--- isoft_linkknown_ui/src/components/Common/decorate/MoveLine.vue
+++ isoft_linkknown_ui/src/components/Common/decorate/MoveLine.vue
@@ -20,16 +20,16 @@
   @keyframes dh_fly {
     0%   {color:red; transform: rotate(0deg);left:5%; top:0;}
     25%  {color:yellow;transform: rotate(180deg); left:100%; top:0;}
-    50%  {color:blue;transform: rotate(360deg); left:100%; top:450%;}
-    75%  {color:green;transform: rotate(180deg); left:5%; top:450%;}
+    50%  {color:blue;transform: rotate(360deg); left:100%; top:135px;}
+    75%  {color:green;transform: rotate(180deg); left:5%; top:135px;}
     100% {color:red;transform: rotate(0deg); left:5%; top:0;}
   }
 
   @-webkit-keyframes dh_fly {
     0%   {color:red; transform: rotate(0deg);left:5%; top:0;}
     25%  {color:yellow;transform: rotate(180deg); left:100%; top:0;}
-    50%  {color:blue;transform: rotate(360deg); left:100%; top:450%;}
-    75%  {color:green;transform: rotate(180deg); left:5%; top:450%;}
+    50%  {color:blue;transform: rotate(360deg); left:100%; top:135px;}
+    75%  {color:green;transform: rotate(180deg); left:5%; top:135px;}
     100% {color:red;transform: rotate(0deg); left:5%; top:0;}
   }
 </style>
