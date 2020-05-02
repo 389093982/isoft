abfdfa0eb33a6e7d4ebf28c74a619299e65dab4a
zhoupeng <1875112921@qq.com>
2020-05-02 18:30:34 +0800

    统一彩色线的组件 SepLine

--- isoft_linkknown_ui/src/components/Common/SepLine.vue
+++ isoft_linkknown_ui/src/components/Common/SepLine.vue
@@ -1,9 +1,10 @@
 <template>
-  <div style="display: flex;">
-    <div style="height: 4px;width: 15%;background-color: rgba(255,255,0,0.3);color: white;"></div>
-    <div style="height: 4px;width: 20%;background-color: rgba(0,128,0,0.3);color: white;"></div>
-    <div style="height: 4px;width: 30%;background-color: rgba(0,0,255,0.3);color: white;"></div>
-    <div style="height: 4px;width: 35%;background-color: rgba(255,0,0,0.3);color: white;"></div>
+  <!--彩色分底线-->
+  <div style="width: 100%;display: flex;margin-top: 5px">
+    <div style="width: 20%;height: 2px;background-color: rgba(255,105,0,0.42)"></div>
+    <div style="width: 30%;height: 2px;background-color: rgba(216,137,255,0.6)"></div>
+    <div style="width: 40%;height: 2px;background-color: rgba(65,255,57,0.51)"></div>
+    <div style="width: 10%;height: 2px;background-color: rgba(255,162,187,0.55)"></div>
   </div>
 </template>
 
--- isoft_linkknown_ui/src/components/Contact/ContactList.vue
+++ isoft_linkknown_ui/src/components/Contact/ContactList.vue
@@ -12,12 +12,7 @@
     </Row>
 
     <!--彩色分底线-->
-    <div style="width: 100%;display: flex;margin-top: 5px">
-      <div style="width: 20%;height: 2px;background-color: rgba(255,105,0,0.42)"></div>
-      <div style="width: 30%;height: 2px;background-color: rgba(216,137,255,0.6)"></div>
-      <div style="width: 40%;height: 2px;background-color: rgba(65,255,57,0.51)"></div>
-      <div style="width: 10%;height: 2px;background-color: rgba(255,162,187,0.55)"></div>
-    </div>
+    <SepLine></SepLine>
 
     <div class="content" style="display: flex;">
       <!--左侧-->
@@ -129,10 +124,11 @@
   import {AddContactMessage, GetContactMessage, GetContactUserList} from "../../api"
   import {getNickName} from "../../tools/sso";
   import LearningDiary from "../ILearning/LearningDiary";
+  import SepLine from "../Common/SepLine";
 
   export default {
     name: "ContactList",
-    components: {LearningDiary},
+    components: {SepLine, LearningDiary},
     data() {
       return {
         sendMsgText: '',
