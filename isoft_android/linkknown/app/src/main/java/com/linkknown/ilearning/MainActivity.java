4bed9adcf210b9257db2d759c6e852700153d80a
389093982 <389093982@qq.com>
2020-05-06 18:21:35 +0800

    z001 - 【安卓】代码重构

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/MainActivity.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/MainActivity.java
@@ -3,35 +3,32 @@ package com.linkknown.ilearning;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 import com.jeremyliao.liveeventbus.LiveEventBus;
 import com.linkknown.ilearning.activity.HomeActivity;
 import com.linkknown.ilearning.activity.LoginActivity;
 import com.linkknown.ilearning.activity.RegistActivity;
-import com.linkknown.ilearning.adapter.MainActivityFragmentAdapter;
-import com.linkknown.ilearning.fragment.ClassifyFragment;
-import com.linkknown.ilearning.fragment.HomeFragment;
-import com.linkknown.ilearning.fragment.MineFragment;
-import com.linkknown.ilearning.service.UserService;
+import com.linkknown.ilearning.fragment.SpaceFragment;
 import com.linkknown.ilearning.model.LoginUserResponse;
+import com.linkknown.ilearning.service.UserService;
 import com.linkknown.ilearning.util.ui.UIUtils;
-import com.linkknown.ilearning.viewpage.MainActivityViewPager;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -48,6 +45,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.drawer_layout)
     public DrawerLayout drawer;
 
+    // 存储首页对应的三个片段
+    private List<Fragment> mFragments = new ArrayList<>();
+    List<String> titles = new ArrayList<>();
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+
     // 用户登录之后显示用户头像及昵称
     private LinearLayout userHeaderInfoLayout;
 
@@ -56,21 +61,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     // 未登录时显示提示信息的布局
     private RelativeLayout unLoginLayout;
-
-    // 存储首页对应的三个片段
-    private List<Fragment> mFragments;
-
-    @BindView(R.id.mViewPage)
-    public MainActivityViewPager mViewPage;
-    // 底部按钮组
-    @BindView(R.id.radioGroup)
-    public RadioGroup radioGroup;
-    @BindView(R.id.imageButton0)
-    public RadioButton rb_home;
-    @BindView(R.id.imageButton1)
-    public RadioButton rb_category;
-    @BindView(R.id.imageButton2)
-    public RadioButton rb_mine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,41 +75,65 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.imageButton0:
-                mViewPage.setCurrentItem(0);
-                break;
-            case R.id.imageButton1:
-                mViewPage.setCurrentItem(1);
-                break;
-            case R.id.imageButton2:
-                mViewPage.setCurrentItem(2);
-                break;
-            default:
-                break;
-        }
-        // 设置按钮组样式
-        this.setRadioGroupStatus();
-    }
 
-    //设置选中和未选择的状态
-    private void setRadioGroupStatus() {
-        this.setRadioButtonStatus(this.rb_home);
-        this.setRadioButtonStatus(this.rb_category);
-        this.setRadioButtonStatus(this.rb_mine);
-    }
-
-    private void setRadioButtonStatus(RadioButton rb) {
-        if (rb.isChecked()) {
-            rb.setTextColor(ContextCompat.getColor(this, R.color.button_press));
-        } else {
-            rb.setTextColor(ContextCompat.getColor(this, R.color.button_normal));
-        }
     }
 
     private void init () {
         initFragment();
         initNavigation();
+    }
+
+    private void initFragment () {
+        // 创建 fragment
+        SpaceFragment spaceFragment1 = new SpaceFragment();
+        SpaceFragment spaceFragment2 = new SpaceFragment();
+        SpaceFragment spaceFragment3 = new SpaceFragment();
+        SpaceFragment spaceFragment4 = new SpaceFragment();
+        SpaceFragment spaceFragment5 = new SpaceFragment();
+        SpaceFragment spaceFragment6 = new SpaceFragment();
+        SpaceFragment spaceFragment7 = new SpaceFragment();
+
+        mFragments.add(spaceFragment1);
+        mFragments.add(spaceFragment2);
+        mFragments.add(spaceFragment3);
+        mFragments.add(spaceFragment4);
+        mFragments.add(spaceFragment5);
+        mFragments.add(spaceFragment6);
+        mFragments.add(spaceFragment7);
+
+        // title 限制 2 个字
+        titles.add("首页");
+        titles.add("圈子");
+        titles.add("热门");
+        titles.add("分类");
+        titles.add("关注");
+        titles.add("发现");
+        titles.add("推荐");
+
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                super.destroyItem(container, position, object);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles.get(position);
+            }
+        });
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void initNavigation () {
@@ -156,43 +170,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
     }
 
-    private void initFragment () {
-        mFragments = new ArrayList<>();
-        // 创建 3 个片段
-        mFragments.add(new HomeFragment());
-        mFragments.add(new ClassifyFragment());
-        mFragments.add(new MineFragment());
-
-        MainActivityFragmentAdapter fragmentAdapter = new MainActivityFragmentAdapter(getSupportFragmentManager(), mFragments);
-        mViewPage.setAdapter(fragmentAdapter);
-        //预渲染页面数量
-        mViewPage.setOffscreenPageLimit(2);
-        //禁用滑动
-        mViewPage.setOnTouchListener((v, event) -> true);
-
-        radioGroup.setBackgroundColor(ContextCompat.getColor(this, R.color.light_gray2));
-
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            switch (checkedId) {
-                case R.id.imageButton0:
-                    // 获取颜色使用 ContextCompat.getColor 方法
-                    radioGroup.setBackgroundColor(ContextCompat.getColor(this, R.color.light_gray2));
-                    mViewPage.setCurrentItem(0);
-                    break;
-                case R.id.imageButton1:
-                    radioGroup.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
-                    mViewPage.setCurrentItem(1);
-                    break;
-                case R.id.imageButton2:
-                    radioGroup.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
-                    mViewPage.setCurrentItem(2);
-                    break;
-                default:
-                    break;
-            }
-        });
-    }
-
     // navigation view 点击事件
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -204,17 +181,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //            mViewPage.setCurrentItem(0);
 //            //底部ImageView点击之后变色
 //            rb_home.setChecked(true);
-            setRadioGroupStatus();
+//            setRadioGroupStatus();
         } else if (id == R.id.fenlei) {
-            mViewPage.setCurrentItem(1);
-            //底部ImageView点击之后变色
-            rb_category.setChecked(true);
-            setRadioGroupStatus();
+//            mViewPage.setCurrentItem(1);
+//            //底部ImageView点击之后变色
+//            rb_category.setChecked(true);
+//            setRadioGroupStatus();
         } else if (id == R.id.wode) {
-            mViewPage.setCurrentItem(2);
-            //底部ImageView点击之后变色
-            rb_mine.setChecked(true);
-            setRadioGroupStatus();
+//            mViewPage.setCurrentItem(2);
+//            //底部ImageView点击之后变色
+//            rb_mine.setChecked(true);
+//            setRadioGroupStatus();
         } else if (id == R.id.denglu) {
 //            //intent跳转
 //            if (LoginCheckUtil.isLogin(this)) {
