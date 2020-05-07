443778d2a88777a96e61505abd3d59a9a5ebbd2b
zhoupeng <1875112921@qq.com>
2020-05-07 08:43:47 +0800

    gitlogin.js里面配置移动到配置里面

--- isoft_linkknown_ui/config/dev.env.js
+++ isoft_linkknown_ui/config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  CLIENT_ID:'"af46aec6d4529d7cd7fd"',
+  CLIENT_SECRET:'"26ed56923769e65599573e85bfe76d5e6740c838"',
 })
--- isoft_linkknown_ui/config/prod.env.js
+++ isoft_linkknown_ui/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  CLIENT_ID:'"7168f3568d2c6d3f6403"',
+  CLIENT_SECRET:'"85e9c5226b5beddc5469298225acc79101975032"',
 }
