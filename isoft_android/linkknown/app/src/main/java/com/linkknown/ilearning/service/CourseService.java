9564428988208799910aa314e2b0384f88b28e91
389093982 <389093982@qq.com>
2020-05-08 09:40:04 +0800

    z001 - 【安卓】样式优化

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/service/CourseService.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/service/CourseService.java
@@ -4,9 +4,13 @@ import android.util.Log;
 
 import com.jeremyliao.liveeventbus.LiveEventBus;
 import com.linkknown.ilearning.factory.LinkKnownApiFactory;
+import com.linkknown.ilearning.model.CourseMetaResponse;
 import com.linkknown.ilearning.model.CourseSearchResponse;
 
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -14,6 +18,43 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class CourseService {
+
+    public static void getHotCourseRecommend () {
+        LinkKnownApiFactory.getLinkKnownApi().getHotCourseRecommend()
+                .subscribeOn(Schedulers.io())                   // 请求在新的线程中执行
+                .observeOn(AndroidSchedulers.mainThread())      // 切换到主线程运行
+                .subscribe(new Observer<CourseMetaResponse>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(CourseMetaResponse courseMetaResponse) {
+                        if (courseMetaResponse.isSuccess() && courseMetaResponse.getCourses() != null) {
+                            LiveEventBus.get("courseMetaResponse", CourseMetaResponse.class).post(courseMetaResponse);
+                        } else {
+                            Log.e("onNext =>", "系统异常,请联系管理员~");
+                            LiveEventBus.get("courseMetaResponse", CourseMetaResponse.class).post(courseMetaResponse);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("onError =>", e.getMessage());
+                        CourseMetaResponse courseMetaResponse = new CourseMetaResponse();
+                        courseMetaResponse.setErrorMsg("数据加载失败!");
+                        LiveEventBus.get("courseMetaResponse", CourseMetaResponse.class).post(courseMetaResponse);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
 
     public static void search(String search) {
         LinkKnownApiFactory.getLinkKnownApi().searchCourseList(search)
