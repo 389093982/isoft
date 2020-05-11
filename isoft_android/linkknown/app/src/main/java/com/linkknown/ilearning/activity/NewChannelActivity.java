b7f3e205581687b0a83b553127384ea6fcd7792f
389093982 <389093982@qq.com>
2020-05-11 14:53:18 +0800

    z001 - 【安卓】toolbar 功能实现

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/NewChannelActivity.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/NewChannelActivity.java
@@ -1,16 +1,31 @@
 package com.linkknown.ilearning.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.linkknown.ilearning.R;
 
-public class NewChannelActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+
+public class NewChannelActivity extends BaseActivity {
+
+    private Context mContext;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_channel);
+        mContext = this;
+        ButterKnife.bind(this);
+
+        init();
+    }
+
+    private void init () {
+        toolbar = findViewById(R.id.toolbar);
+        initToolBar(toolbar, true, "拖拽排序");
     }
 }
