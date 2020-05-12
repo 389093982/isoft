3ad9cef03f3a06476ce7ddad0e4ee7dae3e6e55c
389093982 <389093982@qq.com>
2020-05-12 09:06:49 +0800

    z001 - 【安卓】添加设置页面,评论列表页面支持下拉刷新

---
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.linkknown.ilearning.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.linkknown.ilearning.R;
+
+// 设置页面
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
