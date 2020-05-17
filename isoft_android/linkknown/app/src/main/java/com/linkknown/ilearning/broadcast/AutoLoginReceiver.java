be0caee0dee072e0ba4d94849fb669669059c71c
389093982 <389093982@qq.com>
2020-05-17 18:11:56 +0800

    z001 - 【安卓】全局对话框样式优化

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/broadcast/AutoLoginReceiver.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/broadcast/AutoLoginReceiver.java
@@ -3,6 +3,7 @@ package com.linkknown.ilearning.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AlertDialog;
@@ -21,8 +22,22 @@ public class AutoLoginReceiver extends BroadcastReceiver {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(context, R.style.LinkKnownDialog);
         alertDialog.setView(R.layout.dialog_login_confirm);
         AlertDialog dialog = alertDialog.create();
-        // 需要把对话框的类型设为 TYPE_SYSTEM_ALERT,否则对话框无法在广播接收器里弹出
-        // dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+
+        setPermissionType(dialog);
+
+        // true 代表点击空白可消失,false代表点击空白哦不可消失,不能点击外边和返回键取消对话框
+        dialog.setCancelable(false);
+
         dialog.show();
+    }
+
+    private void setPermissionType(AlertDialog dialog) {
+        //只有这样才能弹框
+        // 需要把对话框的类型设为 TYPE_SYSTEM_ALERT,否则对话框无法在广播接收器里弹出
+        if (Build.VERSION.SDK_INT >= 26) {    // 8.0 新特性
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+        }else{
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        }
     }
 }
