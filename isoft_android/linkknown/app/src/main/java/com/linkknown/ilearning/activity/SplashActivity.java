75d2b389f9ca7fdabeb61f4b01041fbc637de8d7
389093982 <389093982@qq.com>
2020-06-04 16:27:42 +0800

    z001 - 【安卓】开屏页面样式优化

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/SplashActivity.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/SplashActivity.java
@@ -42,7 +42,6 @@ public class SplashActivity extends AppCompatActivity {
 
     @BindView(R.id.imageView)
     public ImageView imageView;
-    public TextBannerView tipTextbanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
