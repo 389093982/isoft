c9d27e04ef8b08f215750801bdbf6709634eea8b
389093982 <389093982@qq.com>
2020-06-06 17:50:17 +0800

    z001 - 【安卓】推荐页重构 + 无网络检测

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/fragment/TuijianFragment.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/fragment/TuijianFragment.java
@@ -1,34 +1,20 @@
 package com.linkknown.ilearning.fragment;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.GridView;
 
-import androidx.annotation.Nullable;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import androidx.viewpager.widget.ViewPager;
-
-import com.linkknown.ilearning.Constants;
 import com.linkknown.ilearning.R;
-import com.linkknown.ilearning.activity.CouponCenterActivity;
 import com.linkknown.ilearning.activity.CouponReceiveCenterActivity;
 import com.linkknown.ilearning.activity.HuodongActivity;
 import com.linkknown.ilearning.adapter.GlideImageLoader;
-import com.linkknown.ilearning.adapter.HomePageGridViewAdapter;
-import com.linkknown.ilearning.adapter.HomePageViewPagerAdapter;
-import com.linkknown.ilearning.model.Catalog;
 import com.linkknown.ilearning.util.NetUtil;
 import com.linkknown.ilearning.util.ui.ToastUtil;
 import com.linkknown.ilearning.util.ui.UIUtils;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.Transformer;
-import com.youth.banner.listener.OnBannerListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,21 +23,11 @@ public class TuijianFragment extends BaseLazyLoadFragment {
 
     private Context mContext;
 
-    @BindView(R.id.viewPager)
-    public ViewPager viewPager;
-
     // 轮播图部分
     @BindView(R.id.banner)
     public Banner banner;
     // 存放轮播图所有图片
     private ArrayList<Integer> bannerImageList;
-
-    // GridView 作为一个 View 对象添加到 ViewPager 集合中
-    private List<View> viewPagerList;
-
-    // 下拉刷新的layout
-    @BindView(R.id.refreshLayout)
-    public SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void initView(View mRootView) {
@@ -62,14 +38,6 @@ public class TuijianFragment extends BaseLazyLoadFragment {
         initBannerView();
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        this.initData();
-
-
-        refreshLayout.setOnRefreshListener(() -> ToastUtil.showText(mContext, "onRefresh..."));
-    }
 
     @Override
     public void initData () {
@@ -78,11 +46,6 @@ public class TuijianFragment extends BaseLazyLoadFragment {
             ToastUtil.showText(mContext, "亲，网络异常，无法获取数据请检查网络！");
             return;
         }
-        // 初始化热门课程分类
-        initHotCategorys();
-
-//            //获取热评商品
-//            initCommodityList(Constants.HOME_PAGE_LIMIT, 0, true);
     }
 
     @Override
@@ -131,48 +94,4 @@ public class TuijianFragment extends BaseLazyLoadFragment {
         });
         banner.setImages(bannerImageList).start();
     }
-    private void initHotCategorys () {
-        List<Catalog> listData = new ArrayList<>();
-        for (int i=0; i<30; i++) {
-            Catalog catalog = new Catalog();
-            catalog.setCatalogName("默认分类1");
-            catalog.setCatalogIcon("https://img-blog.csdn.net/20180420104431654");
-            listData.add(catalog);
-        }
-        initHotCategorysViewPage(listData);
-    }
-
-    private void initHotCategorysViewPage (List<Catalog> listData) {
-        LayoutInflater inflater = LayoutInflater.from(this.getContext());
-        int totalPage = (int) Math.ceil(listData.size() * 1.0 / Constants.DEFAULT_PAGE_SIZE);
-        viewPagerList = new ArrayList<>();
-        for (int i = 0; i < totalPage; i++) {
-            //每个页面都是inflate出一个新实例
-            GridView gridView = (GridView) inflater.inflate(R.layout.gridview_layout, viewPager, false);
-            gridView.setAdapter(new HomePageGridViewAdapter(this.getContext(), listData, i, Constants.DEFAULT_PAGE_SIZE));
-            //每一个GridView作为一个View对象添加到ViewPager集合中
-            viewPagerList.add(gridView);
-        }
-        viewPager.setAdapter(new HomePageViewPagerAdapter(viewPagerList));
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-    }
-
 }
