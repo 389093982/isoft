e8e87dfd4c7906682f7b37208c0621a2f02dfa71
389093982 <389093982@qq.com>
2020-06-07 08:46:32 +0800

    z001 - 【安卓】我的页面重构

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/fragment/MineFragment.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/fragment/MineFragment.java
@@ -11,13 +11,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.jeremyliao.liveeventbus.LiveEventBus;
 import com.linkknown.ilearning.R;
 import com.linkknown.ilearning.activity.AboutUsActivity;
@@ -27,15 +24,23 @@ import com.linkknown.ilearning.activity.CourseOrderActivity;
 import com.linkknown.ilearning.activity.HuodongActivity;
 import com.linkknown.ilearning.activity.MessageInfoActivity;
 import com.linkknown.ilearning.activity.SettingActivity;
+import com.linkknown.ilearning.adapter.GlideImageLoader;
 import com.linkknown.ilearning.model.LoginUserResponse;
 import com.linkknown.ilearning.util.ui.UIUtils;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
 
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MineFragment extends Fragment implements View.OnClickListener {
+
+    @BindView(R.id.banner)
+    public Banner banner;
 
     @BindView(R.id.iv_coupon)
     public ImageView iv_coupon;
@@ -46,11 +51,11 @@ public class MineFragment extends Fragment implements View.OnClickListener {
     @BindView(R.id.iv_huodong)
     public ImageView iv_huodong;
 
+    @BindView(R.id.userInfoLayout)
+    public LinearLayout userInfoLayout;
     // 登录用户头像和用户名
     @BindView(R.id.small_headerIcon)
     public ImageView small_headerIcon;
-    @BindView(R.id.big_headerIcon)
-    public ImageView big_headerIcon;
 
     //购物车
     @BindView(R.id.menuShoppingCart)
@@ -116,9 +121,10 @@ public class MineFragment extends Fragment implements View.OnClickListener {
 //            StrictMode.setVmPolicy(builder.build());
 //            builder.detectFileUriExposure();
 //        }
-        bindMenuDrawnStart();
 
-        initAppBar();
+        initBanner();
+
+        bindMenuDrawnStart();
 
         // 初始化登录相关信息
         initLoginInfo();
@@ -132,6 +138,29 @@ public class MineFragment extends Fragment implements View.OnClickListener {
         menuSettingLayout.setOnClickListener(this);
     }
 
+    // 初始化顶部轮播图组件
+    private void initBanner() {
+        banner.clearAnimation();
+        //设置 banner 样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+
+        // 存放轮播图所有图片
+        ArrayList<Integer> bannerImageList = new ArrayList<>();
+        ArrayList<String> bannerTitleList = new ArrayList<>();
+        //清空旧数据
+        bannerImageList.clear();
+        bannerTitleList.clear();
+        bannerImageList.add(R.drawable.banner_biancheng);
+        bannerImageList.add(R.drawable.banner_shaoer);
+        bannerTitleList.add("编程思维");
+        bannerTitleList.add("少儿编程");
+        banner.setImages(bannerImageList);
+        banner.setBannerTitles(bannerTitleList);
+        banner.start();
+    }
+
     private void initMenuImageView () {
         // 点击调往对应页面
         iv_coupon.setOnClickListener(v -> UIUtils.gotoActivity(mContext, CouponCenterActivity.class));
@@ -143,19 +172,17 @@ public class MineFragment extends Fragment implements View.OnClickListener {
     private void initLoginInfo () {
         LiveEventBus.get("loginUserResponse", LoginUserResponse.class).observe(this, loginUserResponse -> {
             if (StringUtils.isEmpty(loginUserResponse.getErrorMsg()) && StringUtils.isNotEmpty(loginUserResponse.getUserName())) {
+                userInfoLayout.setVisibility(View.VISIBLE);
                 small_headerIcon.setVisibility(View.VISIBLE);
                 // 登录成功
                 Glide.with(getContext())
                         .load(UIUtils.replaceMediaUrl(loginUserResponse.getHeaderIcon()))
                         .apply(new RequestOptions().placeholder(R.drawable.loading).error(R.drawable.error_image))
                         .into(small_headerIcon);
-                Glide.with(getContext())
-                        .load(UIUtils.replaceMediaUrl(loginUserResponse.getHeaderIcon()))
-                        .apply(new RequestOptions().placeholder(R.drawable.loading).error(R.drawable.error_image))
-                        .into(big_headerIcon);
             } else {
+                userInfoLayout.setVisibility(View.GONE);
                 // 登录失败
-                small_headerIcon.setVisibility(View.INVISIBLE);
+                small_headerIcon.setVisibility(View.GONE);
             }
         });
     }
@@ -180,50 +207,10 @@ public class MineFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    @BindView(R.id.appbar)
-    public AppBarLayout appbar;
-    @BindView(R.id.collapsing_toolbar)
-    public CollapsingToolbarLayout collapsing_toolbar;
-    @BindView(R.id.minePageToolBar)
-    public Toolbar minePageToolBar;
-
-    private CollapsingToolbarLayoutState state;
-    // 先写一个枚举定义出CollapsingToolbarLayout展开、折叠、中间，这三种状态。
-    private enum CollapsingToolbarLayoutState {
-        EXPANDED,
-        COLLAPSED,
-        INTERNEDIATE
-    }
-
-    private void initAppBar () {
-        appbar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
-            if (verticalOffset == 0) {
-                if (state != CollapsingToolbarLayoutState.EXPANDED) {
-                    state = CollapsingToolbarLayoutState.EXPANDED;//修改状态标记为展开
-                    collapsing_toolbar.setTitle("EXPANDED");//设置title为EXPANDED
-                }
-            } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
-                if (state != CollapsingToolbarLayoutState.COLLAPSED) {
-                    collapsing_toolbar.setTitle("");//设置title不显示
-                    minePageToolBar.setVisibility(View.VISIBLE);//隐藏 toolbar
-                    state = CollapsingToolbarLayoutState.COLLAPSED;//修改状态标记为折叠
-                }
-            } else {
-                if (state != CollapsingToolbarLayoutState.INTERNEDIATE) {
-                    if(state == CollapsingToolbarLayoutState.COLLAPSED){
-                        minePageToolBar.setVisibility(View.GONE);//由折叠变为中间状态时隐藏 toolbar
-                    }
-                    collapsing_toolbar.setTitle("INTERNEDIATE");//设置title为INTERNEDIATE
-                    state = CollapsingToolbarLayoutState.INTERNEDIATE;//修改状态标记为中间
-                }
-            }
-        });
-    }
-
     /**
      * 菜单前面添加图标
      */
-    public void bindMenuDrawnStart(){
+    public void bindMenuDrawnStart() {
         UIUtils.setTextViewDrawbleImg(mContext, menuShoppingCartTextView, R.drawable.ic_shopping_cart, 0, 2, 40, 42);
         UIUtils.setTextViewDrawbleImg(mContext, menuOrderTextView, R.drawable.ic_order2, 0, 2, 40, 42);
         UIUtils.setTextViewDrawbleImg(mContext, menuAdviseTextView, R.drawable.ic_advise, 0, 2, 40, 42);
@@ -234,6 +221,4 @@ public class MineFragment extends Fragment implements View.OnClickListener {
 
         UIUtils.setTextViewDrawbleImg(mContext, menuSettingLayoutTextView, R.drawable.ic_setup, 0, 2, 40, 42);
     }
-
-
 }
