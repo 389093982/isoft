4cca978a03d499023777aa2858816aef354b536c
389093982 <389093982@qq.com>
2020-06-09 11:05:07 +0800

    z001 - 【安卓】section 去除

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/popup/HotSearchPopView.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/popup/HotSearchPopView.java
@@ -1,35 +1,22 @@
 package com.linkknown.ilearning.popup;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.flexbox.FlexboxLayout;
 import com.linkknown.ilearning.R;
-import com.linkknown.ilearning.section.CommonTagSection;
-import com.linkknown.ilearning.section.ShowAndCloseMoreSection;
-import com.linkknown.ilearning.util.CommonUtil;
-import com.linkknown.ilearning.util.ui.UIUtils;
+import com.linkknown.ilearning.widget.CommonTagView;
+import com.linkknown.ilearning.widget.ShowAndCloseMoreView;
 import com.lxj.xpopup.core.BottomPopupView;
 import com.lxj.xpopup.util.XPopupUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-
-import butterknife.BindView;
-import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 
 public class HotSearchPopView extends BottomPopupView {
 
-    private SectionedRecyclerViewAdapter tagRecyclerViewAdapter;
     List<String> showTagList = new ArrayList<>();
     List<String> hideTagList = new ArrayList<>();
 
@@ -51,31 +38,32 @@ public class HotSearchPopView extends BottomPopupView {
     @Override
     protected void onCreate() {
         super.onCreate();
-
-        RecyclerView showTagView = findViewById(R.id.showTagView);
-
-        tagRecyclerViewAdapter = new SectionedRecyclerViewAdapter();
         showTagList = new ArrayList<>(Arrays.asList("java1", "java2","java3", "java4","java5", "java6","java7","java8"));
-        CommonTagSection showCommonTagSection = new CommonTagSection(mContext, showTagList);
-        CommonTagSection hideCommonTagSection = new CommonTagSection(mContext, hideTagList);
-        ShowAndCloseMoreSection showAndCloseMoreSection = new ShowAndCloseMoreSection(mContext, new ShowAndCloseMoreSection.ClickListener() {
+        hideTagList = new ArrayList<>(Arrays.asList("go1", "go2","go3", "go4","go5", "go6","go7", "go8"));
+
+        CommonTagView showTagView = findViewById(R.id.showTagView);
+        CommonTagView hideTagView = findViewById(R.id.hideTagView);
+        showTagView.setList(showTagList);
+        hideTagView.setList(hideTagList);
+        CommonTagView.CallBackListener listener = text -> {
+            this.listener.onConfirm(text);
+        };
+        showTagView.setCallBackListener(listener);
+        hideTagView.setCallBackListener(listener);
+        hideTagView.setVisibility(View.GONE);
+
+        ShowAndCloseMoreView showAndCloseMoreView = findViewById(R.id.showAndCloseMore);
+        showAndCloseMoreView.setCallBackListener(new ShowAndCloseMoreView.CallBackListener() {
             @Override
             public void showMore() {
-                hideTagList.clear();
-                hideTagList.addAll(new ArrayList<>(Arrays.asList("go1", "go2","go3", "go4","go5", "go6","go7", "go8")));
-                tagRecyclerViewAdapter.notifyDataSetChanged();
+                hideTagView.setVisibility(View.VISIBLE);
             }
+
             @Override
             public void closeMore() {
-                hideTagList.clear();
-                tagRecyclerViewAdapter.notifyDataSetChanged();
+                hideTagView.setVisibility(View.GONE);
             }
         });
-        tagRecyclerViewAdapter.addSection(showCommonTagSection);
-        tagRecyclerViewAdapter.addSection(hideCommonTagSection);
-        tagRecyclerViewAdapter.addSection(showAndCloseMoreSection);
-        showTagView.setLayoutManager(new LinearLayoutManager(mContext));
-        showTagView.setAdapter(tagRecyclerViewAdapter);
     }
 
     @Override
