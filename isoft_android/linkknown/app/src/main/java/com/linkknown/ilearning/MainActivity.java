a2645d779b836e1931bb65e9835b464c4279ae3c
zhoupeng <1875112921@qq.com>
2020-06-10 17:42:55 +0800

    登录注册ok

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/MainActivity.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
             }else if (id == R.id.shoppingCart) {
                 UIUtils.gotoActivity(mContext, ShoppingCartActivity.class);
             }
-            else if (id == R.id.denglu) {
+            else if (id == R.id.login) {
 //            //intent跳转
 //            if (LoginCheckUtil.isLogin(this)) {
 //                Toast.makeText(this, "您已登录过了，请先注销", Toast.LENGTH_SHORT).show();
