3c38943a7e18253b23171fef444fb2766d8b9028
389093982 <389093982@qq.com>
2020-06-12 08:03:21 +0800

    z001 - 【安卓】AgentWeb 库引入

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/HuodongActivity.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/activity/HuodongActivity.java
@@ -2,9 +2,14 @@ package com.linkknown.ilearning.activity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.WebChromeClient;
 import com.linkknown.ilearning.R;
 
 import butterknife.BindView;
@@ -16,6 +21,10 @@ public class HuodongActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.huodongLayout)
+    LinearLayout huodongLayout;
+
+    private AgentWeb mAgentWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,30 @@ public class HuodongActivity extends BaseActivity {
     }
 
     private void initView () {
-        initToolBar(toolbar, true, "活动中心");
+        initAgentWebView();
+    }
+
+    private void initAgentWebView() {
+
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent((LinearLayout) huodongLayout, new LinearLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator()
+                .createAgentWeb()
+                .ready()
+                .go("http://www.jd.com");
+
+        //获取网页的标题
+        mAgentWeb.getWebCreator().getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                if (!TextUtils.isEmpty(title)) {
+                    // 设置 toolbar
+                    initToolBar(toolbar, true, title);
+                }
+                super.onReceivedTitle(view, title);
+            }
+        });
+
     }
 }
+
