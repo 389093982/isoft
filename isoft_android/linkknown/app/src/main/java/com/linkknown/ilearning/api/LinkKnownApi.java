448a8aedfb1c6edf90adf9a59ce0fe779b6eca4c
389093982 <389093982@qq.com>
2020-06-14 16:00:15 +0800

    [Author:z001 - Reason: 考试模块提交]

--- isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/api/LinkKnownApi.java
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/api/LinkKnownApi.java
@@ -14,6 +14,7 @@ import com.linkknown.ilearning.model.FavoriteCountResponse;
 import com.linkknown.ilearning.model.FavoriteResponse;
 import com.linkknown.ilearning.model.HistoryResponse;
 import com.linkknown.ilearning.model.IsFavoriteResponse;
+import com.linkknown.ilearning.model.KaoshiClassifyResponse;
 import com.linkknown.ilearning.model.LoginUserResponse;
 import com.linkknown.ilearning.model.MessageListResponse;
 import com.linkknown.ilearning.model.PayOrderResponse;
@@ -235,6 +236,7 @@ public interface LinkKnownApi {
                                                               @Query("paid_amount") String paid_amount,
                                                               @Query("today") String today);
 
-
-
+    // 查询所有考试分类
+    @GET("/api/iwork/httpservice/isoft_linkknown_api/QueryAllKaoshiClassify")
+    Observable<KaoshiClassifyResponse> queryAllKaoshiClassify();
 }
---
+++ isoft_android/linkknown/app/src/main/java/com/linkknown/ilearning/model/KaoshiClassifyResponse.java
@@ -0,0 +1,24 @@
+package com.linkknown.ilearning.model;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class KaoshiClassifyResponse extends BaseResponse {
+
+    private List<KaoshiClassify> kaoshi_classifys;
+
+    @Data
+    public class KaoshiClassify {
+        private String classify_desc;
+        private String classify_name;
+        private String created_by;
+        private Date created_time;
+        private int id;
+    }
+
+}
