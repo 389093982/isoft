eff94ed9eada4d98d0a6fe90a100b26d5de8f772
389093982 <389093982@qq.com>
2020-06-27 11:01:46 +0800

    flutter 项目首页 tab 标签页实现

--- isoft_flutter/linkknown/lib/page/home_page.dart
+++ isoft_flutter/linkknown/lib/page/home_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:linkknown/widgets/common_tab.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,13 +7,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+
+
+  List<CommonTabViewModel> viewModels = [
+    CommonTabViewModel(title: '免费', widget: Text("宠物卡片")),
+    CommonTabViewModel(title: '付费', widget: Text("宠物卡片")),
+    CommonTabViewModel(title: '全部', widget: Text("宠物卡片")),
+    CommonTabViewModel(title: '推荐', widget: Text("宠物卡片")),
+    CommonTabViewModel(title: '会员', widget: Text("宠物卡片")),
+  ].map((item) => CommonTabViewModel(
+    title: item.title,
+    widget: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[item.widget],
+    ),
+  )).toList();
+
+  TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    this.tabController = new TabController(length: viewModels.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
+    return CommonTabs(
+      title: '基础组件',
+      viewModels: viewModels,
+      tabScrollable: false,
+      tabController: this.tabController,
     );
   }
 
