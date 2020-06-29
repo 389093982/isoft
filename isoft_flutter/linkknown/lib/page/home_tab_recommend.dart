387bc5150f167251c732186431d3a725291bc8ed
389093982 <389093982@qq.com>
2020-06-29 15:49:29 +0800

    flutter 项目: 轮播图优化

--- isoft_flutter/linkknown/lib/page/home_tab_recommend.dart
+++ isoft_flutter/linkknown/lib/page/home_tab_recommend.dart
@@ -1,17 +1,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:linkknown/route/routes.dart';
+import 'package:linkknown/utils/utils.dart';
 
 class TabRecommendWidget extends StatefulWidget {
   @override
   _TabRecommenState createState() => _TabRecommenState();
 }
 
+class SwiperData {
+  String imagePath;
+  String redirectUrl;
+
+  SwiperData(this.imagePath, this.redirectUrl);
+
+}
+
 class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderStateMixin {
 
+  List<SwiperData> swperList = [];
+  
   @override
   void initState() {
     super.initState();
+    swperList..add(SwiperData("images/banner_coupon.png", Routes.login))
+    ..add(SwiperData("images/banner_huodong.png", Routes.login));
   }
 
   @override
@@ -26,30 +40,32 @@ class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderSta
       physics: BouncingScrollPhysics(),
       child: Column(
         children: <Widget>[
-          Card(
-            ///设置阴影的深度
-            elevation: 3.0,
-            ///增加圆角
-            shape: new RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            color: Colors.white,
+          Container(
             margin: const EdgeInsets.all(10),
-            child: new Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              height: 80,
+            width: MediaQuery.of(context).size.width,
+            height: 160,
+            alignment: Alignment(0, 0),
+            // 设置圆角属性
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
               child: Swiper(
                 itemBuilder: _swiperBuilder,
-                itemCount: 3,
+                itemCount: swperList.length,
+                scale: 0.99, // 两张图片之间的间隔
                 pagination: new SwiperPagination(
                     builder: DotSwiperPaginationBuilder(
                       color: Colors.black54,
                       activeColor: Colors.white,
+                      size: 5,
+                      activeSize: 5,
                     )),
-                control: new SwiperControl(),
+                control: new SwiperControl(
+                  iconNext: null,
+                  iconPrevious: null,
+                ),
                 scrollDirection: Axis.horizontal,
                 autoplay: true,
-                onTap: (index) => print('点击了第$index个'),
+                onTap: (index) => UIUtils.showToast('点击了第$index个,需要调往xxx页面'),
               ),
             ),
           ),
@@ -113,7 +129,10 @@ class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderSta
   }
 
   Widget _swiperBuilder(BuildContext context, int index) {
-    return Image.asset("images/banner_coupon.png");
+    return Image.asset(
+      swperList[index].imagePath,
+      fit: BoxFit.cover,
+    );
   }
 
 }
