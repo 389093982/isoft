8b594d7b9982de6d7fe2a4be5e8b4c4708a2aed9
389093982 <389093982@qq.com>
2020-07-07 17:03:54 +0800

    flutter 项目: 样式调整

--- isoft_flutter/linkknown/lib/page/advise_edit_page.dart
+++ isoft_flutter/linkknown/lib/page/advise_edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
+import 'package:linkknown/utils/utils.dart';
 import 'package:linkknown/widgets/divider_line.dart';
 
 class AdviseEditPage extends StatefulWidget {
@@ -9,6 +10,9 @@ class AdviseEditPage extends StatefulWidget {
 }
 
 class _AdviseEditPageState extends State<AdviseEditPage> {
+  String adviseType = "";
+  TextEditingController _adviseController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -54,7 +58,7 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
         color: Colors.deepOrangeAccent,
         splashColor: Colors.deepOrange,
         minWidth: double.infinity,
-        onPressed: () {},
+        onPressed: _submit,
         child: Text(
           "提交",
           style: TextStyle(
@@ -66,18 +70,30 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
     );
   }
 
+  _submit () {
+    if (adviseType.isEmpty) {
+      UIUtils.showToast2("请选择反馈类型");
+      return;
+    }
+    if (_adviseController.text.isEmpty) {
+      UIUtils.showToast2("请输入您的反馈内容");
+      return;
+    }
+    UIUtils.showToast2("提交成功");
+  }
+
   Widget getAdivseEditWidget() {
     return Container(
       margin: EdgeInsets.all(ScreenUtil().setWidth(20.0)),
       width: double.infinity,
-      height: ScreenUtil().setHeight(300),
+      height: ScreenUtil().setHeight(400),
       child: TextField(
-//                    controller: _contentController,
-        maxLines: 10,
+        controller: _adviseController,
+        maxLines: 15,
         style: TextStyle(
           color: Colors.black54,
           fontSize: ScreenUtil().setSp(26),
-       ),
+        ),
         decoration: InputDecoration(
           hintText: "请输入您的反馈内容",
           hintStyle: TextStyle(
@@ -85,10 +101,12 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
             fontSize: ScreenUtil().setSp(26),
           ),
           focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: ScreenUtil().setWidth(1)),
+            borderSide:
+                BorderSide(color: Colors.grey, width: ScreenUtil().setWidth(1)),
           ),
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: ScreenUtil().setWidth(1)),
+            borderSide:
+                BorderSide(color: Colors.grey, width: ScreenUtil().setWidth(1)),
           ),
         ),
       ),
@@ -97,7 +115,7 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
 
   Widget getChooseAdviseTypeWidget() {
     return InkWell(
-      onTap: () {},
+      onTap: _showChooseAdivseTypeDialog,
       child: Container(
         height: ScreenUtil().setHeight(100),
         padding: EdgeInsets.symmetric(horizontal: 20),
@@ -116,10 +134,14 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
                         Text(
-                          "feedBack",
-                          style: TextStyle(color: Colors.grey, fontSize: ScreenUtil().setSp(26)),
+                          adviseType,
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: ScreenUtil().setSp(26)),
                         ),
-                        Padding(padding: EdgeInsets.only(left: ScreenUtil().setWidth(10))),
+                        Padding(
+                            padding: EdgeInsets.only(
+                                left: ScreenUtil().setWidth(10))),
                         Icon(
                           Icons.navigate_next,
                           color: Colors.grey[350],
@@ -131,5 +153,46 @@ class _AdviseEditPageState extends State<AdviseEditPage> {
         ),
       ),
     );
+  }
+
+  Widget getAdviseTypeItem(String text) {
+    return SimpleDialogOption(
+        onPressed: () {
+          Navigator.pop(context);
+          setState(() {
+            adviseType = text;
+          });
+        },
+        child: Container(
+          alignment: Alignment.centerLeft,
+          height: ScreenUtil().setHeight(80.0),
+          child: Text(
+            text,
+            style: TextStyle(
+                color: Colors.black54, fontSize: ScreenUtil().setSp(26.0)),
+          ),
+        ));
+  }
+
+  _showChooseAdivseTypeDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text(
+              "请选择反馈类型",
+              style: TextStyle(
+                  color: Colors.black54, fontSize: ScreenUtil().setSp(28.0)),
+            ),
+            children: <Widget>[
+              getAdviseTypeItem("功能异常"),
+              DividerLineView(),
+              getAdviseTypeItem("优化建议"),
+              DividerLineView(),
+              getAdviseTypeItem("其它"),
+            ],
+          );
+        });
   }
 }
--- isoft_flutter/linkknown/lib/utils/utils.dart
+++ isoft_flutter/linkknown/lib/utils/utils.dart
@@ -1,4 +1,6 @@
 
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class UIUtils {
@@ -6,6 +8,16 @@ class UIUtils {
     Fluttertoast.showToast(msg: msg, gravity: ToastGravity.CENTER);
   }
 
+  static void showToast2(String msg) {
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.deepOrangeAccent,
+        textColor: Colors.white,
+        fontSize: ScreenUtil().setSp(28.0));
+  }
 
   static String replaceMediaUrl (String url) {
     return url.replaceFirst("http://localhost:6001", "http://192.168.1.11:6001");
