c6eaebdc4dbb7455d9d49c8ed74349184d3991d8
389093982 <389093982@qq.com>
2020-07-08 16:30:26 +0800

    flutter 项目: 优化

--- isoft_flutter/linkknown/lib/page/advise_page.dart
+++ isoft_flutter/linkknown/lib/page/advise_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyrefresh/bezier_bounce_footer.dart';
-import 'package:flutter_easyrefresh/bezier_circle_header.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
-import 'package:flutter_easyrefresh/material_footer.dart';
-import 'package:flutter_easyrefresh/material_header.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:linkknown/api/linkknown_api.dart';
 import 'package:linkknown/model/advise_list.dart';
@@ -20,45 +17,34 @@ class AdvisePage extends StatefulWidget {
 
 class _AdvisePageState extends State<AdvisePage> {
   List<Advise> adviseList = new List();
-  int page = 0;
   bool isLoading = false; //是否正在请求新数据
-  bool showMore = false; //是否显示底部加载中提示
+  bool canShowEmptyFlag = false; // 是否可以显示空布局,只有等第一次网络请求结束后才可以显示
+  Paginator paginator;
   EasyRefreshController _easyRefreshController = EasyRefreshController();
 
   @override
   void initState() {
     super.initState();
-
-    // 发送网络请求加载数据
-    initData();
   }
 
-  void initData() {
-    loadPageData(1, 10);
-  }
-
-  void loadPageData(int current_page, int offset) {
-    if (isLoading) {
+  void loadPageData(int current_page, int offset) async {
+    if (isLoading){
       return;
     }
-    setState(() {
-      isLoading = true;
-      page = current_page;
-    });
-    LinkKnownApi.queryPageAdvise(current_page, offset)
-        .then((adviseListResponse) {
-      if (adviseListResponse.status == "SUCCESS") {
+    isLoading = true;
+    AdviseListResponse adviseListResponse = await LinkKnownApi.queryPageAdvise(current_page, offset);
+    if (adviseListResponse.status == "SUCCESS") {
+      setState(() {
         if (current_page == 1) {
           adviseList.clear();
         }
         adviseList.addAll(adviseListResponse.advises);
-      }
 
-      setState(() {
-        isLoading = false;
-        showMore = false;
+        paginator = adviseListResponse.paginator;
       });
-    });
+    }
+    isLoading = false;
+    canShowEmptyFlag = true;
   }
 
   @override
@@ -88,9 +74,82 @@ class _AdvisePageState extends State<AdvisePage> {
       ),
       body: Center(
         child: EasyRefresh(
+          firstRefresh: true,
+          firstRefreshWidget: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Center(
+                child: SizedBox(
+              height: 200.0,
+              width: 300.0,
+              child: Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 50.0,
+                      height: 50.0,
+                      child: SpinKitFadingCube(
+                        color: Theme.of(context).primaryColor,
+                        size: 25.0,
+                      ),
+                    ),
+                    Container(
+                      child: Text("正在加载..."),
+                    )
+                  ],
+                ),
+              ),
+            )),
+          ),
           controller: _easyRefreshController,
-          header: BezierCircleHeader(backgroundColor: Colors.deepOrange),
-          footer: BezierBounceFooter(backgroundColor: Colors.deepOrange),
+          header: ClassicalHeader(
+            refreshText: '下拉刷新',
+            refreshReadyText: '准备刷新',
+            refreshingText: '刷新中...',
+            refreshedText: '刷新完成',
+            bgColor: Colors.transparent,
+            textColor: Colors.black87,
+            showInfo: false,
+          ),
+          footer: ClassicalFooter(
+              loadText: '上拉加载',
+              loadReadyText: '准备加载',
+              loadingText: '加载中...',
+              loadedText: '加载完成',
+              noMoreText: '没有更多',
+              bgColor: Colors.transparent,
+              textColor: Colors.black87,
+          ),
+          emptyWidget: canShowEmptyFlag && adviseList.length == 0
+              ? Container(
+            height: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(),
+                  flex: 2,
+                ),
+                SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: Image.asset('images/linkknown.jpg'),
+                ),
+                Text(
+                  "没有数据",
+                  style: TextStyle(fontSize: 16.0, color: Colors.grey[400]),
+                ),
+                Expanded(
+                  child: SizedBox(),
+                  flex: 3,
+                ),
+              ],
+            ),
+          )
+              : null,
           child: ListView.builder(
 //            controller: scrollController,
             itemCount: adviseList.length, //列表长度+底部加载中提示
@@ -111,17 +170,24 @@ class _AdvisePageState extends State<AdvisePage> {
             physics: AlwaysScrollableScrollPhysics(),
           ),
           onRefresh: _onRefresh,
-          onLoad: () async {
-            page++;
-            loadPageData(page, 10);
-          },
+          onLoad: _onLoadMore,
         ),
       ),
     );
   }
 
+  Future<void> _onLoadMore() async {
+    if (paginator != null && paginator.currpage < paginator.totalpages){
+      await Future.delayed(Duration(seconds: 2), () {
+        loadPageData(paginator.currpage + 1, 10);
+      });
+    }
+  }
+
   Future<void> _onRefresh() async {
-    initData();
+    await Future.delayed(Duration(seconds: 2), () {
+      loadPageData(1, 10);
+    });
   }
 
   @override
@@ -129,5 +195,4 @@ class _AdvisePageState extends State<AdvisePage> {
     super.dispose();
 //    scrollController.dispose();
   }
-
 }
