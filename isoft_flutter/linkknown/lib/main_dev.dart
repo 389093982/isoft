5b97892d0cd716fb5aa9b9856f29b27647e3d3bd
zhoupeng <1875112921@qq.com>
2020-07-13 15:25:14 +0800

    本地环境,文件名和地址修改

--- isoft_flutter/linkknown/lib/main_dev.dart
+++ isoft_flutter/linkknown/lib/main_dev.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:linkknown/app_index.dart';
-import 'package:linkknown/config/local_config.dart';
+import 'package:linkknown/config/dev_config.dart';
 
 void main() {
   runAppWithConfig(localConfig);
