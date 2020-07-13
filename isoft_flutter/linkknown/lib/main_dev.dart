f52de83e3da6f9164937195f05a219fee3c9d786
389093982 <389093982@qq.com>
2020-07-13 14:53:34 +0800

    flutter 项目: 多环境配置

---
+++ isoft_flutter/linkknown/lib/main_dev.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:linkknown/app_index.dart';
+import 'package:linkknown/config/dev_config.dart';
+
+void main() {
+  runAppWithConfig(devConfig);
+}
--- isoft_flutter/linkknown/lib/main_local.dart
+++
@@ -1,8 +0,0 @@
-
-
-import 'package:linkknown/app_index.dart';
-import 'package:linkknown/config/local_config.dart';
-
-void main() {
-  runAppWithConfig(localConfig);
-}
