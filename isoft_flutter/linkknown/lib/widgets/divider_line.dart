7334705b85ced9f31fba6cc9b29ffedb54ae8a25
389093982 <389093982@qq.com>
2020-07-15 10:49:39 +0800

    flutter 项目: 详情页面功能开发

--- isoft_flutter/linkknown/lib/widgets/divider_line.dart
+++ isoft_flutter/linkknown/lib/widgets/divider_line.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DividerLineView extends StatelessWidget {
+  EdgeInsetsGeometry margin;
+
+  DividerLineView({this.margin});
+
   @override
   Widget build(BuildContext context) {
-    return Divider(
-      color: Colors.grey[350],
-      height: ScreenUtil().setHeight(1.0),
+    return Container(
+      margin: this.margin ?? EdgeInsets.all(0),
+      child: Divider(
+        color: Colors.grey[450],
+        height: ScreenUtil().setHeight(1.0),
+      ),
     );
   }
 }
