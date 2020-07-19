91ed6ce81d316fff3aac03e0c39ff6810db20557
389093982 <389093982@qq.com>
2020-07-19 12:05:48 +0800

    flutter 项目: 优化

--- isoft_flutter/linkknown/lib/page/course_detail.dart
+++ isoft_flutter/linkknown/lib/page/course_detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:linkknown/api/linkknown_api.dart';
-import 'package:linkknown/common/scroll_helper.dart';
 import 'package:linkknown/model/course_detail.dart';
 import 'package:linkknown/page/course_comment.dart';
 import 'package:linkknown/page/course_introduce.dart';
@@ -48,69 +47,67 @@ class _CourseDetailPageState extends State<CourseDetailPage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ScrollConfiguration(
-        behavior: NoShadowScrollBehavior(),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            // AppBar 和 SliverAppBar 是Material Design中的 App Bar，也就是 Android 中的 Toolbar
-            // SliverAppBar是一个与 CustomScrollView 结合使用的material design风格的标题栏 .
-            // 不同于AppBar, 它可以展开或收缩.
-            SliverAppBar(
-              leading: Container(
-                  // 绘制返回键
-                  margin: EdgeInsets.all(10), // 设置边距
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      size: 20,
-                    ),
-                    onPressed: () {
-                      // 返回上一页
-                      NavigatorUtil.goBack(context);
-                    },
-                  )),
-              // appBar是否置顶
-              pinned: true,
-              elevation: 0,
-              expandedHeight: 200,
-              // 一个显示在 AppBar 下方的控件，高度和 AppBar 高度一样，可以实现一些特殊的效果，该属性通常在 SliverAppBar 中使用
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text(course != null ? course.courseName : ""),
-                background: Image.network(
-                  UIUtils.replaceMediaUrl(
-                      course != null ? course.smallImage : ""),
-                  fit: BoxFit.fill,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              // AppBar 和 SliverAppBar 是Material Design中的 App Bar，也就是 Android 中的 Toolbar
+              // SliverAppBar是一个与 CustomScrollView 结合使用的material design风格的标题栏 .
+              // 不同于AppBar, 它可以展开或收缩.
+              SliverAppBar(
+                leading: Container(
+                    // 绘制返回键
+                    margin: EdgeInsets.all(10), // 设置边距
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        size: 20,
+                      ),
+                      onPressed: () {
+                        // 返回上一页
+                        NavigatorUtil.goBack(context);
+                      },
+                    )),
+                // appBar是否置顶
+                pinned: true,
+                elevation: 0,
+                expandedHeight: 200,
+                // 一个显示在 AppBar 下方的控件，高度和 AppBar 高度一样，可以实现一些特殊的效果，该属性通常在 SliverAppBar 中使用
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(course != null ? course.courseName : ""),
+                  background: Image.network(
+                    UIUtils.replaceMediaUrl(
+                        course != null ? course.smallImage : ""),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
-            ),
-            // SliverPersistentHeader最重要的一个属性是SliverPersistentHeaderDelegate，为此我们需要实现一个类继承自SliverPersistentHeaderDelegate
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: StickyTabBarDelegate(
-                child: TabBar(
-                  labelColor: Colors.white,
-                  indicatorColor: Colors.white,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  controller: this.tabController,
-                  tabs: <Widget>[
-                    Tab(text: '简介'),
-                    Tab(text: '评论'),
-                  ],
+              // SliverPersistentHeader最重要的一个属性是SliverPersistentHeaderDelegate，为此我们需要实现一个类继承自SliverPersistentHeaderDelegate
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: StickyTabBarDelegate(
+                  child: TabBar(
+                    unselectedLabelColor: Colors.black,
+                    labelColor: Colors.red[400],
+                    indicatorColor: Colors.red[400],
+                    indicatorSize: TabBarIndicatorSize.label,
+                    controller: this.tabController,
+                    tabs: <Widget>[
+                      Tab(text: '简介'),
+                      Tab(text: '评论'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            // SliverFillRemaining 创建填充视口中剩余空间的小条
-            SliverFillRemaining(
-              child: TabBarView(
-                controller: this.tabController,
-                children: <Widget>[
-                  Center(child: CourseIntroduceWidget(course, cVideos)),
-                  CourseCommentWidget(),
-                ],
-              ),
-            ),
-          ],
+            ];
+          },
+          body: TabBarView(controller: this.tabController, children: [
+            Center(child: CourseIntroduceWidget(course, cVideos)),
+            CourseCommentWidget(),
+          ]),
         ),
       ),
     );
@@ -131,7 +128,6 @@ class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Material(
-      color: Colors.red,
       child: this.child,
     );
   }
