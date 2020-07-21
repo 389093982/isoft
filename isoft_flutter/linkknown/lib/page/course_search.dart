ac44a05a42c9e25f846e8be625cfe040d9296a10
389093982 <389093982@qq.com>
2020-07-21 15:20:11 +0800

    flutter 项目: 完善热门搜索

--- isoft_flutter/linkknown/lib/page/course_search.dart
+++ isoft_flutter/linkknown/lib/page/course_search.dart
@@ -1,13 +1,10 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:linkknown/page/course_filter.dart';
-import 'package:linkknown/utils/utils.dart';
+import 'package:linkknown/widgets/common_label.dart';
 import 'package:linkknown/widgets/common_search.dart';
 
 class CourseSearchPage extends StatefulWidget {
-
   String search;
   String isCharge;
 
@@ -15,7 +12,6 @@ class CourseSearchPage extends StatefulWidget {
 
   @override
   _CourseSearchPageState createState() => _CourseSearchPageState();
-
 }
 
 class _CourseSearchPageState extends State<CourseSearchPage> {
@@ -54,27 +50,137 @@ class _CourseSearchPageState extends State<CourseSearchPage> {
                 },
               ),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.only(right: 10),
-                  child: Text("热门搜索", style: TextStyle(color: Colors.grey),),
+                  child: InkWell(
+                    onTap: (){
+                      showHotOrHistory('hot');
+                    },
+                    child: Text(
+                      "热门搜索",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
                 Container(
                   margin: EdgeInsets.only(right: 10),
-                  child: Text("搜索历史", style: TextStyle(color: Colors.grey),),
+                  child: InkWell(
+                    onTap: (){
+                      showHotOrHistory('history');
+                    },
+                    child: Text(
+                      "搜索历史",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Expanded(
               child: CourseFilterWidget(widget.search, widget.isCharge),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  List hotSearch1 = ["前端", "后端", "数据库", "基础", "运维", "测试", "游戏", "全栈", "人工智能", "github"];
+  List hotSearch2 = ["大数据", "云计算", "爬虫", "算法", "自动化", "区块链", "深度学习"];
+
+  bool showHotMore = true;
+
+  showHotOrHistory (String type) {
+    showModalBottomSheet(context: context, builder: (BuildContext context) {
+      return StatefulBuilder(
+          builder:(context1, state) {///这里的state就是setState
+            if (type == "hot") {
+              return showHotWidget(state, context);
+            } else {
+              return showHistoryWidget(state, context);
+            }
+          });
+    });
+  }
+
+  /// Flutter 更新ModalBottomSheet中的状态，为了区分把 setState 改个名字
+  Widget showHotWidget (Function _setState, BuildContext context) {
+    List hotSearch = [];
+    hotSearch..addAll(hotSearch1)..addAll(showHotMore ? [] : hotSearch2);
+
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("大家都在搜"),
+          SizedBox(height: 10,),
+          // Wrap是一个可以使子控件自动换行的控件，默认的方向是水平的
+          Wrap(
+            spacing: 5, //主轴上子控件的间距
+            runSpacing: 5, //交叉轴上子控件之间的间距
+            children: List.generate(hotSearch.length, (index) {
+              return CommonLabel.getCommonLabel2(hotSearch[index]);
+            }),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Divider(thickness: 2, color: Colors.grey,),
+                ),
+                SizedBox(width: 10,),
+                InkWell(
+                  onTap: () {
+                    _setState(() {      ///为了区分把setState改个名字
+                      showHotMore = !showHotMore;
+                    });
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[600],
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: Icon(showHotMore ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up, size: 20, color: Colors.white,),
+                        ),
+                        SizedBox(width: 10,),
+                        Text(showHotMore ? "查看更多" : "收起"),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: Divider(thickness: 2, color: Colors.grey,),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget showHistoryWidget (Function _setState, BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      child: Text("history"),
     );
   }
 }
