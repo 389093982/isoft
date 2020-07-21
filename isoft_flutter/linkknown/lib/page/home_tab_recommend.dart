21068556e79e0d45f7cf9e39d61a3ee814a47b6a
389093982 <389093982@qq.com>
2020-07-21 10:58:10 +0800

    flutter 项目: 优化

--- isoft_flutter/linkknown/lib/page/home_tab_recommend.dart
+++ isoft_flutter/linkknown/lib/page/home_tab_recommend.dart
@@ -3,30 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:linkknown/route/routes.dart';
 import 'package:linkknown/utils/utils.dart';
+import 'package:linkknown/widgets/swiper_info.dart';
 
 class TabRecommendWidget extends StatefulWidget {
   @override
   _TabRecommenState createState() => _TabRecommenState();
 }
 
-class SwiperData {
-  String imagePath;
-  String redirectUrl;
-
-  SwiperData(this.imagePath, this.redirectUrl);
-
-}
 
 class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderStateMixin {
-
-  List<SwiperData> swperList = [];
-  
-  @override
-  void initState() {
-    super.initState();
-    swperList..add(SwiperData("images/banner_coupon.png", Routes.login))
-    ..add(SwiperData("images/banner_huodong.png", Routes.login));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,35 +25,7 @@ class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderSta
       physics: BouncingScrollPhysics(),
       child: Column(
         children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width,
-            height: 160,
-            alignment: Alignment(0, 0),
-            // 设置圆角属性
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Swiper(
-                itemBuilder: _swiperBuilder,
-                itemCount: swperList.length,
-                scale: 0.99, // 两张图片之间的间隔
-                pagination: new SwiperPagination(
-                    builder: DotSwiperPaginationBuilder(
-                      color: Colors.black54,
-                      activeColor: Colors.white,
-                      size: 5,
-                      activeSize: 5,
-                    )),
-                control: new SwiperControl(
-                  iconNext: null,
-                  iconPrevious: null,
-                ),
-                scrollDirection: Axis.horizontal,
-                autoplay: true,
-                onTap: (index) => UIUtils.showToast('点击了第$index个,需要调往xxx页面'),
-              ),
-            ),
-          ),
+          SwiperDataWidget(),
           Text("好无聊奥~~"),
           Text("好无聊奥~~"),
           Text("好无聊奥~~"),
@@ -79,11 +36,6 @@ class _TabRecommenState extends State<TabRecommendWidget> with TickerProviderSta
     );
   }
 
-  Widget _swiperBuilder(BuildContext context, int index) {
-    return Image.asset(
-      swperList[index].imagePath,
-      fit: BoxFit.cover,
-    );
-  }
+
 
 }
---
+++ isoft_flutter/linkknown/lib/widgets/swiper_info.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:linkknown/route/routes.dart';
+import 'package:linkknown/utils/utils.dart';
+
+class SwiperData {
+  String imagePath;
+  String redirectUrl;
+
+  SwiperData(this.imagePath, this.redirectUrl);
+}
+
+class SwiperDataWidget extends StatefulWidget {
+
+  EdgeInsets margin;
+  double height;
+
+  SwiperDataWidget({this.margin, this.height});
+
+  @override
+  _SwiperDataState createState() => _SwiperDataState();
+}
+
+class _SwiperDataState extends State<SwiperDataWidget> {
+
+  List<SwiperData> swperList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    swperList..add(SwiperData("images/banner_coupon.png", Routes.login))
+      ..add(SwiperData("images/banner_huodong.png", Routes.login));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: widget.margin ?? const EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width,
+      height: widget.height ?? 160,
+      alignment: Alignment(0, 0),
+      // 设置圆角属性
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Swiper(
+          itemBuilder: _swiperBuilder,
+          itemCount: swperList.length,
+          scale: 0.99, // 两张图片之间的间隔
+          pagination: new SwiperPagination(
+              builder: DotSwiperPaginationBuilder(
+                color: Colors.black54,
+                activeColor: Colors.white,
+                size: 5,
+                activeSize: 5,
+              )),
+          control: new SwiperControl(
+            iconNext: null,
+            iconPrevious: null,
+          ),
+          scrollDirection: Axis.horizontal,
+          autoplay: true,
+          onTap: (index) => UIUtils.showToast('点击了第$index个,需要调往xxx页面'),
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperBuilder(BuildContext context, int index) {
+    return Image.asset(
+      swperList[index].imagePath,
+      fit: BoxFit.cover,
+    );
+  }
+
+}
