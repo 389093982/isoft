0b51dc4d9605f88ec5b24bc2bb3e820fc9badf58
zhoupeng <1875112921@qq.com>
2020-08-02 11:25:14 +0800

    分类：课程搜索框优化

--- isoft_flutter/linkknown/lib/widgets/common_search.dart
+++ isoft_flutter/linkknown/lib/widgets/common_search.dart
@@ -20,38 +20,56 @@ class SearchInputWidget extends StatelessWidget {
       child: Theme(
           data: new ThemeData(primaryColor: Colors.grey),
           child: Container(
-            padding: EdgeInsets.only(top: 5.0, left: 5.0),
             decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: new BorderRadius.all(new Radius.circular(40.0)),
-                border: Border(
-                  bottom: BorderSide(width: 1.0, color: Colors.black12),
-                  top: BorderSide(width: 1.0, color: Colors.black12),
-                  right: BorderSide(width: 1.0, color: Colors.black12),
-                  left: BorderSide(width: 1.0, color: Colors.black12),
-                )),
-            child: TextField(
-              controller: searchInputController,
-              cursorColor: Colors.red, // 光标颜色
-              // 默认设置
-              decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 5.0, horizontal: 35.0),
-                  border: InputBorder.none,
-                  suffixIcon: InkWell(
-                    onTap: () {
-                      handleSearch(searchInputController.text);
-                    },
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.red,
-                    ),
-                  ),
-                  hintText: "输入搜索内容",
-                  hintStyle:
-                      new TextStyle(fontSize: 14, color: Colors.grey[400])),
-              style: new TextStyle(fontSize: 14, color: Colors.black),
             ),
+            child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 40,
+                        //maxWidth: 300
+                      ),
+                      child: TextField(
+                        controller: searchInputController,
+                        //最大行数
+                        maxLines: 1,
+                        //输入文本的样式
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                        //光标颜色
+                        cursorColor: Colors.red,
+                        //光标宽度
+                        cursorWidth: 2.0,
+                        // 默认设置
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 12,top:0),//光标距离左侧距离
+                          hintText: "输入搜索内容",
+                          hintStyle: new TextStyle(fontSize: 14, color: Colors.grey[400]),
+                          enabledBorder: OutlineInputBorder(//未点击输入框的效果
+                            borderSide: BorderSide(
+                              color: Colors.white, //边框颜色
+                              width: 2, //宽度为2
+                            ),
+                            borderRadius: BorderRadius.circular(30),//四个角弧度
+                          ),
+                          focusedBorder: OutlineInputBorder(//点击输入框后的效果
+                            borderSide: BorderSide(
+                              color: Colors.white, //边框颜色
+                              width: 2, //宽度为2
+                            ),
+                            borderRadius: BorderRadius.circular(30),//四个角弧度
+                          ),
+                          suffixIcon: InkWell(
+                            onTap: () {
+                              handleSearch(searchInputController.text);
+                            },
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      )
+                  ),
           )),
     );
   }
