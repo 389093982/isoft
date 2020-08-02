b789a14e67d7c676d55f001a4ee9324ad763acdb
zhoupeng <1875112921@qq.com>
2020-08-02 16:11:16 +0800

    更换头像ok

--- isoft_flutter/linkknown/lib/page/modify_header.dart
+++ isoft_flutter/linkknown/lib/page/modify_header.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:linkknown/api/linkknown_api.dart';
+import 'package:linkknown/common/error.dart';
 import 'package:linkknown/common/styles/textstyles.dart';
 import 'package:linkknown/model/base.dart';
 import 'package:linkknown/model/fileupload_response.dart';
 import 'package:linkknown/utils/login_util.dart';
+import 'package:linkknown/utils/navigator_util.dart';
+import 'package:linkknown/utils/string_util.dart';
 import 'package:linkknown/utils/utils.dart';
 import 'package:linkknown/widgets/common_button.dart';
 import 'package:linkknown/widgets/image_select.dart';
@@ -15,10 +18,36 @@ class ModifyHeaderPage extends StatefulWidget {
 
 class ModifyHeaderPageState extends State<ModifyHeaderPage> {
 
+  String loginUserSamllIcon;
   String filePath;
 
   @override
+  void initState() {
+    super.initState();
+    queryLoginUserSamllIcon();
+  }
+
+  queryLoginUserSamllIcon() async {
+    String loginUserName = await LoginUtil.getLoginUserName();
+    if(loginUserName!=null && loginUserName!=""){
+      LinkKnownApi.getUserDetail(loginUserName).then((GetUserDetailResponse) {
+        if (GetUserDetailResponse != null && GetUserDetailResponse.status == "SUCCESS") {
+          loginUserSamllIcon = GetUserDetailResponse.user.smallIcon;
+          setState(() {
+          //刷新
+          });
+        }
+      }).catchError((e) {
+        UIUtils.showToast((e as LinkKnownError).errorMsg);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if(StringUtil.checkEmpty(loginUserSamllIcon)){
+      return Text("");
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text("点击图片更换头像"),
@@ -27,22 +56,13 @@ class ModifyHeaderPageState extends State<ModifyHeaderPage> {
         padding: EdgeInsets.all(60),
         child: ListView(
           children: <Widget>[
+            SizedBox(height: 100,),
             Row(
               children: <Widget>[
                 Expanded(
-                  child:
-                  ImageSelectWidget(
-                    label: "从相册中获取",
-                    source: ImageSelectWidget.SOURCE_GALLERY,
-                    onSelect: (_filePath){
-                      filePath = _filePath;
-                    },
-                  ),
-                ),
-                Expanded(
                   child: ImageSelectWidget(
-                    label: "拍照上传头像",
-                    source: ImageSelectWidget.SOURCE_CAMERA,
+                    label: "上传头像",
+                    currentSmallIcon:loginUserSamllIcon,
                     onSelect: (_filePath){
                       filePath = _filePath;
                     },
@@ -50,7 +70,7 @@ class ModifyHeaderPageState extends State<ModifyHeaderPage> {
                 ),
               ],
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 40,),
             CommonButton(
               callback: modifyHeader,
               content: '提交',
@@ -63,12 +83,17 @@ class ModifyHeaderPageState extends State<ModifyHeaderPage> {
   }
 
   modifyHeader () async {
+    if(StringUtil.checkEmpty(filePath)){
+      UIUtils.showToast("没有更新头像..");
+      return;
+    }
     FileUploadResponse fileUploadResponse = await LinkKnownApi.fileUpload("user", "small_icon", filePath);
     if (fileUploadResponse.status == "SUCCESS") {
       String userName = await LoginUtil.getUserName();
       BaseResponse baseResponse = await LinkKnownApi.updateUserIcon(userName, fileUploadResponse.fileServerPath);
       if (baseResponse.status == "SUCCESS") {
         UIUtils.showToast2("设置成功！");
+        NavigatorUtil.goBack(context);
       } else {
         UIUtils.showToast2("设置失败！");
       }
